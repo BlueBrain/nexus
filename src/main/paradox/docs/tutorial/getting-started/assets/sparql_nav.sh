791c443ee34b9930325522fa382936b9dbaf6efb
Mohameth François Sy <mfsy@tsf-492-wpa-4-090.epfl.ch>
2019-01-22 15:14:27 +0100

    Added sparql queries

---
+++ src/main/paradox/docs/tutorial/getting-started/assets/sparql_nav.sh
@@ -0,0 +1,10 @@
+# Select 5 movies sorted by title in descending order
+$ nexus search --view $NEXUS-URL/views/amld2019/movielens/graph/sparql -d \
+'PREFIX vocab: <$NEXUS-URL/vocabs/amld2019/movielens/>
+ Select ?m ?title
+   WHERE  {
+      ?m     a       vocab:Movie.
+      ?m vocab:title ?title.
+}
+ORDER BY vocab:title DESC
+LIMIT 5'
