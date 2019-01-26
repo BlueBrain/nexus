c15c33aaddf9167f465e68fcb857bef89df09beb
MFSY <mohamethfrancois.sy@gmail.com>
2019-01-26 14:21:46 +0100

    updated site

---
+++ docs/tutorial/getting-started/assets/analytics_elastic.sh
@@ -0,0 +1,26 @@
+# Average rating score for movies tagged as funny
+nexus views query-sparql --data \
+'
+PREFIX vocab: <https://nexus-sandbox.io/v1/vocabs/testamld2019/movielens2/>
+PREFIX nxv: <https://bluebrain.github.io/nexus/vocabulary/>
+
+Select  DISTINCT ?movieId (AVG(?rating) AS ?score)
+ WHERE  {
+    # Select movies
+    ?movie a vocab:Movie.
+
+    # Select their movieId value
+    ?movie vocab:movieId ?movieId.
+
+    # Keep movies with 'funny' tags
+    ?tagnode vocab:movieId ?movieId.
+    ?tagnode vocab:tag "funny".
+
+    # Keep movies with ratings
+    ?ratingNode vocab:movieId ?movidId.
+    ?ratingNode vocab:rating ?rating.
+
+}
+GROUP BY ?movieId'
+
+
