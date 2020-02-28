80fe5eee0bdf645c5f917a002ba8d8488a8a2c21
Didac <didac.monteromendez@epfl.ch>
2020-02-28 16:58:08 +0100

    Added project knowledge to SparqlClient (#1061)

--- cli/shared/src/main/scala/ch/epfl/bluebrain/nexus/cli/SparqlClient.scala
+++ cli/shared/src/main/scala/ch/epfl/bluebrain/nexus/cli/SparqlClient.scala
@@ -1,14 +1,33 @@
 package ch.epfl.bluebrain.nexus.cli
 
 import ch.epfl.bluebrain.nexus.cli.SparqlClient.SparqlError
-import ch.epfl.bluebrain.nexus.cli.types.SparqlResults
+import ch.epfl.bluebrain.nexus.cli.types.{Label, SparqlResults}
+import org.http4s.Uri
 
 trait SparqlClient[F[_]] {
+
+  private val defaultSparqlView =
+    Uri.unsafeFromString("https://bluebrain.github.io/nexus/vocabulary/defaultSparqlIndex")
+
   /**
-   * Performs a SPARQL query
-   * @param value the SPARQL query
-   */
-  def query(value: String): F[Either[SparqlError, SparqlResults]]
+    * Performs a SPARQL query on the default view of the passed organization and project.
+    *
+    * @param organization the organization label
+    * @param project      the project label
+    * @param value        the SPARQL query
+    */
+  def query(organization: Label, project: Label, value: String): F[Either[SparqlError, SparqlResults]] =
+    query(organization, project, defaultSparqlView, value)
+
+  /**
+    * Performs a SPARQL query on the passed view, organization and project.
+    *
+    * @param organization the organization label
+    * @param project      the project label
+    * @param viewId       the view @id value
+    * @param value        the SPARQL query
+    */
+  def query(organization: Label, project: Label, viewId: Uri, value: String): F[Either[SparqlError, SparqlResults]]
 }
 
 object SparqlClient {
