731319b17b567a9f72e557e81cd8ff1790c6e386
Didac Montero <didac.monteromendez@epfl.ch>
2020-03-03 10:14:58 +0100

    Fixed formatting

--- cli/shared/src/main/scala/ch/epfl/bluebrain/nexus/cli/types/EventEnvelope.scala
+++ cli/shared/src/main/scala/ch/epfl/bluebrain/nexus/cli/types/EventEnvelope.scala
@@ -1,9 +1,9 @@
 package ch.epfl.bluebrain.nexus.cli.types
 
 /**
- * Event wrapper adding its metadata.
- *
- * @param offset the offset of the event
- * @param event  the event value
+  * Event wrapper adding its metadata.
+  *
+  * @param offset the offset of the event
+  * @param event  the event value
  **/
 final case class EventEnvelope(offset: Offset, event: Event)
