c257a1ff03a4864b92cf1f89fbdfb20d0cb45edc
Didac Montero <didac.monteromendez@epfl.ch>
2020-05-27 16:20:44 +0200

    Removed IAM from service

--- service/src/main/scala/ch/epfl/bluebrain/nexus/ResourceRejection.scala
+++
@@ -1,8 +0,0 @@
-package ch.epfl.bluebrain.nexus
-
-import akka.http.scaladsl.server.Rejection
-
-/**
-  * Parent type to all resource rejections for iam.
-  */
-trait ResourceRejection extends Rejection with Product with Serializable
