1bf2a8bd7fe60f07c0abb1b8929df7b13dfe04bd
Didac <didac.monteromendez@epfl.ch>
2020-05-27 16:25:18 +0200

    Removed IAM from service (#1206)

--- service/src/main/scala/ch/epfl/bluebrain/nexus/auth/AccessToken.scala
+++
@@ -1,8 +0,0 @@
-package ch.epfl.bluebrain.nexus.auth
-
-/**
-  * A data structure which represents an access token
-  *
-  * @param value the token value
-  */
-final case class AccessToken(value: String)
--- service/src/main/scala/ch/epfl/bluebrain/nexus/syntax/package.scala
+++
@@ -1,8 +0,0 @@
-package ch.epfl.bluebrain.nexus
-
-package object syntax {
-  object all  extends JsonSyntax with PathSyntax
-  object json extends JsonSyntax
-  object path extends PathSyntax
-
-}
