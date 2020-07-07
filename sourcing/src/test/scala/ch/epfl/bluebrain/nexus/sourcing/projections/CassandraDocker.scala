424676120bc7a6c799285b82f627bf7ab17107fb
Bogdan Roman <bogdanromanx@gmail.com>
2020-07-07 14:04:32 +0200

    Updated ProjectionsSpec to use a Cassandra container (#1301)
    * Updated dependencies
    
    * Updated sourcing tests to use a Cassandra container
    
    * Reduced Cassandra memory and configured projections tests to run sequentially

---
+++ sourcing/src/test/scala/ch/epfl/bluebrain/nexus/sourcing/projections/CassandraDocker.scala
@@ -0,0 +1,31 @@
+package ch.epfl.bluebrain.nexus.sourcing.projections
+
+import distage.{ModuleDef, TagK}
+import izumi.distage.docker.ContainerDef
+import izumi.distage.docker.Docker.DockerPort
+
+object CassandraDocker extends ContainerDef {
+  val primaryPort: DockerPort = DockerPort.TCP(9042)
+
+  override def config: Config = {
+    Config(
+      image = "cassandra:3.11.6",
+      ports = Seq(primaryPort),
+      env = Map(
+        "JVM_OPTS"      -> "-Xms1g -Xmx1g",
+        "MAX_HEAP_SIZE" -> "1g",
+        "HEAP_NEWSIZE"  -> "100m"
+      )
+    )
+  }
+}
+
+class CassandraDockerModule[F[_]: TagK] extends ModuleDef {
+  make[CassandraDocker.Container].fromResource {
+    CassandraDocker.make[F]
+  }
+}
+
+object CassandraDockerModule {
+  def apply[F[_]: TagK]: CassandraDockerModule[F] = new CassandraDockerModule[F]
+}
--- sourcing/src/test/scala/ch/epfl/bluebrain/nexus/sourcing/projections/ProjectionsSpec.scala
+++ sourcing/src/test/scala/ch/epfl/bluebrain/nexus/sourcing/projections/ProjectionsSpec.scala
@@ -3,54 +3,93 @@ package ch.epfl.bluebrain.nexus.sourcing.projections
 import akka.actor.ActorSystem
 import akka.persistence.query.Offset
 import akka.stream.scaladsl.Source
-import akka.testkit.{TestKit, TestKitBase}
-import cats.effect.{ContextShift, IO}
-import ch.epfl.bluebrain.nexus.sourcing.projections.Fixture.memoize
-import ch.epfl.bluebrain.nexus.sourcing.projections.ProjectionProgress._
+import akka.stream.{Materializer, SystemMaterializer}
+import cats.effect.{ContextShift, IO, Resource, Timer}
+import cats.implicits._
+import ch.epfl.bluebrain.nexus.sourcing.projections.ProjectionProgress.{NoProgress, OffsetProgress}
 import ch.epfl.bluebrain.nexus.sourcing.projections.ProjectionsSpec.SomeEvent
-import io.circe.generic.auto._
-import org.scalatest.concurrent.Eventually
-import org.scalatest.matchers.should.Matchers
-import org.scalatest.wordspec.AnyWordSpecLike
-import org.scalatest.{BeforeAndAfterAll, DoNotDiscover}
+import com.typesafe.config.ConfigFactory
+import distage.ModuleDef
+import io.circe.{Decoder, Encoder}
+import izumi.distage.docker.Docker
+import izumi.distage.docker.modules.DockerSupportModule
+import izumi.distage.effect.modules.CatsDIEffectModule
+import izumi.distage.model.definition.StandardAxis
+import izumi.distage.plugins.PluginConfig
+import izumi.distage.testkit.TestConfig
+import izumi.distage.testkit.TestConfig.ParallelLevel
+import izumi.distage.testkit.scalatest.DistageSpecScalatest
+import org.scalatest.matchers.should.Matchers.{contain, empty}
 
-import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext
 
-//noinspection TypeAnnotation
-@DoNotDiscover
-class ProjectionsSpec
-    extends TestKitBase
-    with AnyWordSpecLike
-    with Matchers
-    with TestHelpers
-    with IOValues
-    with Eventually
-    with BeforeAndAfterAll {
+class ProjectionsSpec extends DistageSpecScalatest[IO] with TestHelpers with ShouldMatchers {
 
-  implicit override lazy val system: ActorSystem      = SystemBuilder.persistence("ProjectionsSpec")
-  implicit private val contextShift: ContextShift[IO] = IO.contextShift(system.dispatcher)
+  implicit protected val cs: ContextShift[IO] = IO.contextShift(ExecutionContext.global)
+  implicit protected val tm: Timer[IO]        = IO.timer(ExecutionContext.global)
 
-  override protected def afterAll(): Unit = {
-    TestKit.shutdownActorSystem(system)
-    super.afterAll()
-  }
+  override protected def config: TestConfig =
+    TestConfig(
+      pluginConfig = PluginConfig.empty,
+      activation = StandardAxis.testDummyActivation,
+      parallelTests = ParallelLevel.Sequential,
+      moduleOverrides = new ModuleDef {
+        // add docker dependencies and override default configuration
+        include(new DockerSupportModule[IO] overridenBy new ModuleDef {
+          make[Docker.ClientConfig].from {
+            Docker.ClientConfig(
+              readTimeoutMs = 60000, // long timeout for gh actions
+              connectTimeoutMs = 500,
+              allowReuse = true,
+              useRemote = false,
+              useRegistry = true,
+              remote = None,
+              registry = None
+            )
+          }
+        })
+        include(CatsDIEffectModule)
+        include(CassandraDockerModule[IO])
+        make[ActorSystem].fromResource {
+          c: CassandraDocker.Container =>
+            val port   = c.availablePorts.first(CassandraDocker.primaryPort)
+            val config = ConfigFactory
+              .parseString(s"""datastax-java-driver.basic.contact-points = ["${port.hostV4}:${port.port}"]""")
+              .withFallback(ConfigFactory.parseResources("cassandra.conf"))
+              .withFallback(ConfigFactory.load())
+              .resolve()
+            Resource.make(IO.delay(ActorSystem("ProjectionsSpec", config))) { system =>
+              IO.fromFuture(IO(system.terminate())) >> IO.unit
+            }
+        }
+        make[Materializer].from { as: ActorSystem =>
+          SystemMaterializer(as).materializer
+        }
+        make[Projections[IO, SomeEvent]].fromEffect { implicit as: ActorSystem =>
+          Projections[IO, SomeEvent]
+        }
+      },
+      configBaseName = "projections-test"
+    )
 
   "A Projection" should {
     val id            = genString()
     val persistenceId = s"/some/${genString()}"
-    val projections   = memoize(Projections[IO, SomeEvent]).unsafeRunSync()
     val progress      = OffsetProgress(Offset.sequence(42), 42, 42, 0)
 
-    "store progress" in {
-      projections.ioValue.recordProgress(id, progress).ioValue
+    "store and retrieve progress" in { projections: Projections[IO, SomeEvent] =>
+      for {
+        _    <- projections.recordProgress(id, progress)
+        read <- projections.progress(id)
+        _     = read shouldEqual progress
+      } yield ()
     }
 
-    "retrieve stored progress" in {
-      projections.ioValue.progress(id).ioValue shouldEqual progress
-    }
-
-    "retrieve NoProgress for unknown projections" in {
-      projections.ioValue.progress(genString()).ioValue shouldEqual NoProgress
+    "retrieve NoProgress for unknown projections" in { projections: Projections[IO, SomeEvent] =>
+      for {
+        read <- projections.progress(genString())
+        _     = read shouldEqual NoProgress
+      } yield ()
     }
 
     val firstOffset: Offset  = Offset.sequence(42)
@@ -58,38 +97,40 @@ class ProjectionsSpec
     val firstEvent           = SomeEvent(1L, "description")
     val secondEvent          = SomeEvent(2L, "description2")
 
-    "store an event" in {
-      projections.ioValue.recordFailure(id, persistenceId, 1L, firstOffset, firstEvent).ioValue
+    "store and retrieve failures for events" in { (projections: Projections[IO, SomeEvent], m: Materializer) =>
+      implicit val materializer: Materializer = m
+      val expected                            = Seq((firstEvent, firstOffset), (secondEvent, secondOffset))
+      for {
+        _   <- projections.recordFailure(id, persistenceId, 1L, firstOffset, firstEvent)
+        _   <- projections.recordFailure(id, persistenceId, 2L, secondOffset, secondEvent)
+        log <- logOf(projections.failures(id))
+        _    = log should contain theSameElementsInOrderAs expected
+      } yield ()
     }
 
-    "store another event" in {
-      projections.ioValue.recordFailure(id, persistenceId, 2L, secondOffset, secondEvent).ioValue
-    }
-
-    "retrieve stored events" in {
-      val expected = Seq((firstEvent, firstOffset), (secondEvent, secondOffset))
-      eventually {
-        logOf(projections.ioValue.failures(id)) should contain theSameElementsInOrderAs expected
-      }
-    }
-
-    "retrieve empty list of events for unknown failure log" in {
-      eventually {
-        logOf(projections.ioValue.failures(genString())) shouldBe empty
-      }
+    "retrieve no failures for an unknown projection" in { (projections: Projections[IO, SomeEvent], m: Materializer) =>
+      implicit val materializer: Materializer = m
+      for {
+        log <- logOf(projections.failures(genString()))
+        _    = log shouldBe empty
+      } yield ()
     }
 
   }
 
-  private def logOf(source: Source[(SomeEvent, Offset), _]): Vector[(SomeEvent, Offset)] = {
+  private def logOf(
+      source: Source[(SomeEvent, Offset), _]
+  )(implicit mat: Materializer): IO[Vector[(SomeEvent, Offset)]] = {
     val f = source.runFold(Vector.empty[(SomeEvent, Offset)])(_ :+ _)
-    IO.fromFuture(IO(f)).ioValue
+    IO.fromFuture(IO(f))
   }
-
-  implicit override def patienceConfig: PatienceConfig =
-    PatienceConfig(30.seconds, 50.milliseconds)
 }
 
 object ProjectionsSpec {
   final case class SomeEvent(rev: Long, description: String)
+  object SomeEvent {
+    import io.circe.generic.semiauto._
+    implicit final val encoder: Encoder[SomeEvent] = deriveEncoder[SomeEvent]
+    implicit final val decoder: Decoder[SomeEvent] = deriveDecoder[SomeEvent]
+  }
 }
