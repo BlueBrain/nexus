4d73b3a170ca9ac0659ceaf6ea2d3c97b559fc2e
Simon Dumas <simon.dumas@epfl.ch>
2020-08-12 18:20:23 +0200

    Add some comments

--- sourcing-new/src/main/scala/ch/epfl/bluebrain/nexus/sourcingnew/EventDefinition.scala
+++ sourcing-new/src/main/scala/ch/epfl/bluebrain/nexus/sourcingnew/EventDefinition.scala
@@ -1,20 +1,51 @@
 package ch.epfl.bluebrain.nexus.sourcingnew
 
 /**
-  * Define the behavior of a family of events
-  * @tparam F
-  * @tparam State
-  * @tparam Command
-  * @tparam Event
-  * @tparam Rejection
+  * Description of an event source based entity
+  * @tparam F         the effect type
+  * @tparam State     the state type
+  * @tparam Command   the command type
+  * @tparam Event     the event type
+  * @tparam Rejection the rejection type
   */
 sealed trait EventDefinition[F[_], State, Command, Event, Rejection] {
+
+  /**
+    * The entity type
+    * @return
+    */
   def entityType: String
+
+  /**
+    * The initial state before applying any event
+    * @return
+    */
   def initialState: State
+
+  /**
+    * State transition function; represented as a total function without any effect types;
+    * Should be pure
+    *
+    * @return
+    */
   def next: (State, Event) => State
+
+  /**
+    * Command evaluation function; represented as a function
+    * that returns the evaluation in an arbitrary effect type
+    * May be asynchronous
+    *
+    * @return
+    */
   def evaluate: (State, Command) => F[Either[Rejection, Event]]
 }
 
+/**
+  * Implementation of an [[EventDefinition]]
+  * relying on akka-persistence
+  * @param tagger            the tags to apply to the event
+  * @param snapshotStrategy  the snapshot strategy to apply
+  */
 final case class PersistentEventDefinition[F[_], State, Command, Event, Rejection]
   (
     entityType: String,
@@ -26,7 +57,11 @@ final case class PersistentEventDefinition[F[_], State, Command, Event, Rejectio
     snapshotStrategy: SnapshotStrategy = SnapshotStrategy.NoSnapshot
   ) extends EventDefinition[F, State, Command, Event, Rejection]
 
-
+/**
+  * A transient implementation of an [[EventDefinition]]
+  *
+  * In case of restart or crash, the state will be lost
+  */
 final case class TransientEventDefinition[F[_], State, Command, Event, Rejection]
 (
   entityType: String,
