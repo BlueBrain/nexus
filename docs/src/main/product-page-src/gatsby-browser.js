6ee6e9301c7a813c682cd5a40c7933618cc317a6
Pirman Kenneth William <github@kenny.wtf>
2020-08-14 14:14:28 +0200

    add gtm lib

--- docs/src/main/product-page-src/gatsby-browser.js
+++ docs/src/main/product-page-src/gatsby-browser.js
@@ -1,9 +1,11 @@
 import svgify from "./src/libs/svgify"
+import gtm from "./src/libs/gtm"
 
 import "./src/styles/global.css"
 
 export const onClientEntry = () => {
   console.log("We've started!")
+  gtm()
 }
 
 export const onInitialClientRender = () => {
