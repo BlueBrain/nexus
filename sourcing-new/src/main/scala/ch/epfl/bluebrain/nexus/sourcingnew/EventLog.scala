2a9c72ea56d94dfeb302e17e49d722c36d0297ab
Simon Dumas <simon.dumas@epfl.ch>
2020-08-14 12:08:11 +0200

    Refactor EventLog to make it easier to use with projections

--- sourcing-new/src/main/scala/ch/epfl/bluebrain/nexus/sourcingnew/EventLog.scala
+++ sourcing-new/src/main/scala/ch/epfl/bluebrain/nexus/sourcingnew/EventLog.scala
@@ -1,6 +1,7 @@
 package ch.epfl.bluebrain.nexus.sourcingnew
 
 import akka.persistence.query.{EventEnvelope, Offset}
+import ch.epfl.bluebrain.nexus.sourcingnew.projections.Message
 import fs2._
 import izumi.distage.model.definition.With
 
@@ -11,45 +12,41 @@ import scala.reflect.ClassTag
   * A log of ordered events for uniquely identifiable entities.
   *
   * @tparam F[_]       the event log effect type
-  * @tparam Event      the event type
+  * @tparam M      the event type
   */
-trait EventLog[F[_], Event] {
+trait EventLog[F[_], M] {
   def currentPersistenceIds: Stream[F, String]
   def persistenceIds: Stream[F, String]
   def eventsByPersistenceId(persistenceId: String,
                             fromSequenceNr: Long,
-                            toSequenceNr: Long): Stream[F, Event]
+                            toSequenceNr: Long): Stream[F, M]
   def currentEventsByPersistenceId(persistenceId: String,
                                    fromSequenceNr: Long,
-                                   toSequenceNr: Long): Stream[F, Event]
+                                   toSequenceNr: Long): Stream[F, M]
   def eventsByTag(tag: String,
-                  offset: Offset): Stream[F, Event]
+                  offset: Offset): Stream[F, M]
   def currentEventsByTag(tag: String,
-                         offset: Offset):Stream[F, Event]
+                         offset: Offset):Stream[F, M]
 }
 object EventLog {
   import akka.actor.typed.ActorSystem
   import akka.persistence.cassandra.query.scaladsl.CassandraReadJournal
   import akka.persistence.jdbc.query.scaladsl.JdbcReadJournal
-  import akka.persistence.query.scaladsl.ReadJournal
-  import akka.persistence.query.scaladsl._
+  import akka.persistence.query.scaladsl.{ReadJournal, _}
   import akka.persistence.query.{Offset, PersistenceQuery}
   import akka.stream.Materializer
   import akka.stream.scaladsl.Source
-  import cats.implicits._
   import cats.effect.{Async, ContextShift}
   import streamz.converter._
 
   /**
     * Implementation of [[EventLog]] based on Akka Persistence
-    * @param readJournal
-    * @param contextShift$F$0
-    * @param async$F$1
-    * @param classTag$Event$0
-    * @param as
+    * @param readJournal the underlying akka persistence journal
+    * @param f the function to transform envelopes
+    * @param as the actor system
     * @tparam F[_]       the event log effect type
     * @tparam RJ         the underlying journal typr (Cassandra / JDBC / ...)
-    * @tparam Event      the event type
+    * @tparam M      the event type
     */
   class AkkaEventLog[
     F[_]: ContextShift: Async,
@@ -59,19 +56,11 @@ object EventLog {
       with CurrentEventsByPersistenceIdQuery
       with EventsByTagQuery
       with CurrentEventsByTagQuery,
-    Event: ClassTag](readJournal: RJ)
-                    (implicit as: ActorSystem[Nothing]) extends EventLog[F, Event] {
-
-    private val Event = implicitly[ClassTag[Event]]
+    M](readJournal: RJ, f: EventEnvelope => M)
+                (implicit as: ActorSystem[Nothing]) extends EventLog[F, M] {
 
     implicit val executionContext: ExecutionContext = as.executionContext
     implicit val materializer: Materializer = Materializer.createMaterializer(as)
-
-    private def toEvent(eventEnvelope: EventEnvelope) =
-      eventEnvelope.event match {
-        case Event(ev) => Some(ev)
-        case _         => None
-      }
 
     private def toStream[A](source: Source[A, _]) =
       source.toStream[F]( _ => ())
@@ -82,49 +71,54 @@ object EventLog {
 
     override def eventsByPersistenceId(persistenceId: String,
                                        fromSequenceNr: Long,
-                                       toSequenceNr: Long): Stream[F, Event] =
-      toStream(readJournal.eventsByPersistenceId(persistenceId, fromSequenceNr, toSequenceNr)).mapFilter(toEvent)
+                                       toSequenceNr: Long): Stream[F, M] =
+      toStream(readJournal.eventsByPersistenceId(persistenceId, fromSequenceNr, toSequenceNr)).map(f)
 
     override def currentEventsByPersistenceId(persistenceId: String,
                                               fromSequenceNr: Long,
-                                              toSequenceNr: Long): Stream[F, Event] =
-      toStream(readJournal.currentEventsByPersistenceId(persistenceId, fromSequenceNr, toSequenceNr)).mapFilter(toEvent)
+                                              toSequenceNr: Long): Stream[F, M] =
+      toStream(readJournal.currentEventsByPersistenceId(persistenceId, fromSequenceNr, toSequenceNr)).map(f)
 
     override def eventsByTag(tag: String,
-                             offset: Offset): Stream[F, Event] =
-      toStream(readJournal.eventsByTag(tag, offset)).mapFilter(toEvent)
+                             offset: Offset): Stream[F, M] =
+      toStream(readJournal.eventsByTag(tag, offset)).map(f)
 
     override def currentEventsByTag(tag: String,
-                                    offset: Offset): Stream[F, Event] =
-      toStream(readJournal.currentEventsByTag(tag, offset)).mapFilter(toEvent)
+                                    offset: Offset): Stream[F, M] =
+      toStream(readJournal.currentEventsByTag(tag, offset)).map(f)
   }
 
   trait CassandraEventLogFactory[F[_]] {
 
-    def newEventLog[Event: ClassTag]: EventLog[F, Event] @With[AkkaEventLog[F, CassandraReadJournal, Event]]
+    def newEventLog[M](f: EventEnvelope => M): EventLog[F, M] @With[AkkaEventLog[F, CassandraReadJournal, M]]
+
+    def newProjectionEventLog[A: ClassTag]: EventLog[F, Message[A]] =
+      newEventLog(Message.apply)
   }
 
   class CassandraEventLogFactoryImpl[F[_] : ContextShift : Async]
   (implicit as: ActorSystem[Nothing]) extends CassandraEventLogFactory[F] {
 
-    override def newEventLog[Event: ClassTag]: EventLog[F, Event] =
-      new AkkaEventLog[F, CassandraReadJournal, Event](
-        PersistenceQuery(as).readJournalFor[CassandraReadJournal](CassandraReadJournal.Identifier)
+    override def newEventLog[M](f: EventEnvelope => M): EventLog[F, M] =
+      new AkkaEventLog[F, CassandraReadJournal, M](
+        PersistenceQuery(as).readJournalFor[CassandraReadJournal](CassandraReadJournal.Identifier),
+        f
       )
   }
 
 
   trait JdbcEventLogFactory[F[_]] {
 
-    def newEventLog[Event: ClassTag]: EventLog[F, Event] @With[AkkaEventLog[F, JdbcReadJournal, Event]]
+    def newEventLog[M](f: EventEnvelope => M): EventLog[F, M] @With[AkkaEventLog[F, JdbcReadJournal, M]]
   }
 
   class JdbcEventLogFactoryImpl[F[_] : ContextShift : Async]
       (implicit as: ActorSystem[Nothing]) extends JdbcEventLogFactory[F] {
 
-    override def newEventLog[Event: ClassTag]: EventLog[F, Event] =
-      new AkkaEventLog[F, JdbcReadJournal, Event](
-        PersistenceQuery(as).readJournalFor[JdbcReadJournal](JdbcReadJournal.Identifier)
+    override def newEventLog[M](f: EventEnvelope => M): EventLog[F, M] =
+      new AkkaEventLog[F, JdbcReadJournal, M](
+        PersistenceQuery(as).readJournalFor[JdbcReadJournal](JdbcReadJournal.Identifier),
+        f
       )
   }
 }
