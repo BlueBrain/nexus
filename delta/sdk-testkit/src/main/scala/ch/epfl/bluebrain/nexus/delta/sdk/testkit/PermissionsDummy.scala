f247ba156b762d13e9145ff572a33465ddef324d
Bogdan Roman <bogdanromanx@gmail.com>
2020-09-17 14:43:36 +0200

    Implemented Permissions state machine and PermissionsDummy (#1616)
    * Implemented Permissions state machine
    
    * Addressed feedback and implemented a dummy Permissions

---
+++ delta/sdk-testkit/src/main/scala/ch/epfl/bluebrain/nexus/delta/sdk/testkit/PermissionsDummy.scala
@@ -0,0 +1,149 @@
+package ch.epfl.bluebrain.nexus.delta.sdk.testkit
+
+import cats.effect.concurrent.{Ref, Semaphore}
+import cats.implicits._
+import ch.epfl.bluebrain.nexus.delta.rdf.syntax._
+import ch.epfl.bluebrain.nexus.delta.sdk.model.Identity.Subject
+import ch.epfl.bluebrain.nexus.delta.sdk.model.permissions.PermissionsCommand._
+import ch.epfl.bluebrain.nexus.delta.sdk.model.permissions.PermissionsRejection.RevisionNotFound
+import ch.epfl.bluebrain.nexus.delta.sdk.model.permissions.PermissionsState.Initial
+import ch.epfl.bluebrain.nexus.delta.sdk.model.permissions._
+import ch.epfl.bluebrain.nexus.delta.sdk.testkit.PermissionsDummy._
+import ch.epfl.bluebrain.nexus.delta.sdk.{Permissions, PermissionsResource}
+import monix.bio.{IO, Task, UIO}
+import org.apache.jena.iri.IRI
+
+/**
+  * A dummy Permissions implementation that uses a synchronized in memory journal.
+  *
+ * @param minimum   the minimum set of permissions
+  * @param journal   a ref to the journal
+  * @param semaphore a semaphore for serializing write operations on the journal
+  */
+final class PermissionsDummy private (
+    override val minimum: Set[Permission],
+    journal: Ref[Task, Vector[PermissionsEvent]],
+    semaphore: Semaphore[Task]
+) extends Permissions {
+
+  /**
+    * @return the permissions singleton persistence id
+    */
+  override val persistenceId: String = "permissions"
+
+  /**
+    * @return the current permissions as a resource
+    */
+  override def fetch: UIO[PermissionsResource] =
+    currentState.map(_.toResource(id, minimum))
+
+  /**
+    * @param rev the permissions revision
+    * @return the permissions as a resource at the specified revision
+    */
+  override def fetchAt(rev: Long): IO[RevisionNotFound, PermissionsResource] =
+    stateAt(rev).map(_.toResource(id, minimum))
+
+  /**
+    * Replaces the current collection of permissions with the provided collection.
+    *
+    * @param permissions the permissions to set
+    * @param rev         the last known revision of the resource
+    * @param caller      a reference to the subject that initiated the action
+    * @return the new resource or a description of why the change was rejected
+    */
+  override def replace(
+      permissions: Set[Permission],
+      rev: Long
+  )(implicit caller: Subject): IO[PermissionsRejection, PermissionsResource] =
+    eval(ReplacePermissions(rev, permissions, caller))
+
+  /**
+    * Appends the provided permissions to the current collection of permissions.
+    *
+    * @param permissions the permissions to append
+    * @param rev         the last known revision of the resource
+    * @return the new resource or a description of why the change was rejected
+    */
+  override def append(
+      permissions: Set[Permission],
+      rev: Long
+  )(implicit caller: Subject): IO[PermissionsRejection, PermissionsResource] =
+    eval(AppendPermissions(rev, permissions, caller))
+
+  /**
+    * Subtracts the provided permissions to the current collection of permissions.
+    *
+    * @param permissions the permissions to subtract
+    * @param rev         the last known revision of the resource
+    * @param caller      a reference to the subject that initiated the action
+    * @return the new resource or a description of why the change was rejected
+    */
+  override def subtract(
+      permissions: Set[Permission],
+      rev: Long
+  )(implicit caller: Subject): IO[PermissionsRejection, PermissionsResource] =
+    eval(SubtractPermissions(rev, permissions, caller))
+
+  /**
+    * Removes all but the minimum permissions from the collection of permissions.
+    *
+    * @param rev    the last known revision of the resource
+    * @param caller a reference to the subject that initiated the action
+    * @return the new resource or a description of why the change was rejected
+    */
+  override def delete(rev: Long)(implicit caller: Subject): IO[PermissionsRejection, PermissionsResource] =
+    eval(DeletePermissions(rev, caller))
+
+  private def currentState: UIO[PermissionsState] =
+    journal.get.hideErrors.map { events =>
+      events.foldLeft[PermissionsState](Initial)(Permissions.next(minimum))
+    }
+
+  private def stateAt(rev: Long): IO[RevisionNotFound, PermissionsState] =
+    journal.get.hideErrors.flatMap { events =>
+      if (events.size < rev) IO.raiseError(RevisionNotFound(rev, events.size.toLong))
+      else
+        events
+          .foldLeft[PermissionsState](Initial) {
+            case (state, event) if event.rev <= rev => Permissions.next(minimum)(state, event)
+            case (state, _)                         => state
+          }
+          .pure[UIO]
+    }
+
+  private def eval(cmd: PermissionsCommand): IO[PermissionsRejection, PermissionsResource] = {
+    val result = semaphore.withPermit {
+      for {
+        events     <- journal.get.hideErrors
+        current     = events.foldLeft[PermissionsState](Initial)(Permissions.next(minimum))
+        rejOrEvent <- Permissions.evaluate(minimum)(current, cmd).attempt
+        rejOrState <- rejOrEvent match {
+                        case Left(rej)    => UIO.pure(Left(rej))
+                        case Right(event) =>
+                          journal.set(events :+ event).hideErrors >> UIO.pure(
+                            Right(Permissions.next(minimum)(current, event))
+                          )
+                      }
+      } yield rejOrState
+    }
+    result.hideErrors.flatMap(either => IO.fromEither(either)).map(_.toResource(id, minimum))
+  }
+}
+
+object PermissionsDummy {
+  val id: IRI = iri"http://localhost/v1/permissions"
+
+  /**
+    * Creates a new dummy Permissions implementation.
+    *
+    * @param minimum the minimum set of permissions
+    */
+  final def apply(minimum: Set[Permission]): UIO[PermissionsDummy] = {
+    val dummy = for {
+      ref <- Ref.of[Task, Vector[PermissionsEvent]](Vector.empty)
+      sem <- Semaphore[Task](1L)
+    } yield new PermissionsDummy(minimum, ref, sem)
+    dummy.hideErrors
+  }
+}
---
+++ delta/sdk-testkit/src/test/scala/ch/epfl/bluebrain/nexus/delta/sdk/testkit/PermissionsDummySpec.scala
@@ -0,0 +1,168 @@
+package ch.epfl.bluebrain.nexus.delta.sdk.testkit
+
+import java.time.Instant
+
+import cats.effect.Clock
+import ch.epfl.bluebrain.nexus.delta.rdf.Vocabulary.{nxv, schemas}
+import ch.epfl.bluebrain.nexus.delta.sdk.model.Identity.{Anonymous, Subject}
+import ch.epfl.bluebrain.nexus.delta.sdk.model.ResourceRef.Latest
+import ch.epfl.bluebrain.nexus.delta.sdk.model.permissions.Permission
+import ch.epfl.bluebrain.nexus.delta.sdk.model.permissions.PermissionsRejection._
+import ch.epfl.bluebrain.nexus.delta.sdk.model.{Identity, Label, ResourceF}
+import ch.epfl.bluebrain.nexus.delta.sdk.{Permissions, PermissionsResource}
+import ch.epfl.bluebrain.nexus.testkit.{IOValues, TestHelpers}
+import monix.bio.{IO, UIO}
+import monix.execution.Scheduler
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpecLike
+
+import scala.concurrent.duration.TimeUnit
+
+class PermissionsDummySpec extends AnyWordSpecLike with Matchers with IOValues with TestHelpers {
+
+  val minimum = Set(
+    Permission.unsafe("acls/read"),
+    Permission.unsafe("acls/write"),
+    Permission.unsafe("permissions/read"),
+    Permission.unsafe("permissions/write"),
+    Permission.unsafe("realms/read"),
+    Permission.unsafe("realms/write"),
+    Permission.unsafe("events/read"),
+    Permission.unsafe("organizations/read"),
+    Permission.unsafe("organizations/write"),
+    Permission.unsafe("organizations/create"),
+    Permission.unsafe("projects/read"),
+    Permission.unsafe("projects/write"),
+    Permission.unsafe("projects/create"),
+    Permission.unsafe("resources/read"),
+    Permission.unsafe("resources/write"),
+    Permission.unsafe("resolvers/write"),
+    Permission.unsafe("views/query"),
+    Permission.unsafe("views/write"),
+    Permission.unsafe("schemas/write"),
+    Permission.unsafe("files/write"),
+    Permission.unsafe("storages/write"),
+    Permission.unsafe("archives/write")
+  )
+
+  val read: Permission = Permission.unsafe("permissions/read")
+
+  val perm1: Permission = Permission.unsafe(genString())
+  val perm2: Permission = Permission.unsafe(genString())
+  val perm3: Permission = Permission.unsafe(genString())
+  val perm4: Permission = Permission.unsafe(genString())
+
+  val instant2L: Instant        = Instant.ofEpochMilli(2L)
+  implicit val subject: Subject = Identity.User("user", Label.unsafe("realm"))
+
+  implicit val scheduler: Scheduler = Scheduler.global
+  implicit val fixed2L: Clock[UIO]  = new Clock[UIO] {
+    override def realTime(unit: TimeUnit): UIO[Long]  = IO.pure(unit.convert(2L, unit))
+    override def monotonic(unit: TimeUnit): UIO[Long] = IO.pure(unit.convert(2L, unit))
+  }
+
+  val dummy: Permissions = PermissionsDummy(minimum).accepted
+
+  def resourceFor(
+      rev: Long,
+      permissions: Set[Permission],
+      updatedAt: Instant,
+      updatedBy: Subject
+  ): PermissionsResource =
+    ResourceF(
+      id = PermissionsDummy.id,
+      rev = rev,
+      types = Set(nxv.Permissions),
+      deprecated = false,
+      createdAt = Instant.EPOCH,
+      createdBy = Identity.Anonymous,
+      updatedAt = updatedAt,
+      updatedBy = updatedBy,
+      schema = Latest(schemas.permissions),
+      value = permissions
+    )
+
+  "A PermissionsDummy" should {
+    "return its persistence id" in {
+      dummy.persistenceId shouldEqual "permissions"
+    }
+    "echo the minimum permissions" in {
+      dummy.minimum shouldEqual minimum
+    }
+    "return the minimum permissions set" in {
+      dummy.fetchPermissionSet.accepted shouldEqual minimum
+    }
+    "return the minimum permissions resource" in {
+      val expected = resourceFor(0L, minimum, Instant.EPOCH, Anonymous)
+      dummy.fetch.accepted shouldEqual expected
+    }
+    "fail to delete minimum when initial" in {
+      dummy.delete(0L).rejected shouldEqual CannotDeleteMinimumCollection
+    }
+    "fail to subtract with incorrect rev" in {
+      dummy.subtract(Set(perm1), 1L).rejected shouldEqual IncorrectRev(1L, 0L)
+    }
+    "fail to subtract from minimum" in {
+      dummy.subtract(Set(perm1), 0L).rejected shouldEqual CannotSubtractFromMinimumCollection(minimum)
+    }
+    "fail to subtract undefined permissions" in {
+      dummy.append(Set(perm1), 0L).accepted
+      dummy.fetchPermissionSet.accepted shouldEqual (minimum + perm1)
+      dummy.subtract(Set(perm2), 1L).rejected shouldEqual CannotSubtractUndefinedPermissions(Set(perm2))
+    }
+    "fail to subtract empty permissions" in {
+      dummy.subtract(Set(), 1L).rejected shouldEqual CannotSubtractEmptyCollection
+    }
+    "fail to subtract from minimum collection" in {
+      dummy.subtract(Set(read), 1L).rejected shouldEqual CannotSubtractFromMinimumCollection(minimum)
+    }
+    "subtract a permission" in {
+      dummy.subtract(Set(perm1), 1L).accepted
+      dummy.fetchPermissionSet.accepted shouldEqual minimum
+    }
+    "fail to append with incorrect rev" in {
+      dummy.append(Set(perm1), 0L).rejected shouldEqual IncorrectRev(0L, 2L)
+    }
+    "append permissions" in {
+      dummy.append(Set(perm1, perm2), 2L).accepted
+      dummy.fetchPermissionSet.accepted shouldEqual (minimum ++ Set(perm1, perm2))
+    }
+    "fail to append duplicate permissions" in {
+      dummy.append(Set(perm2), 3L).rejected shouldEqual CannotAppendEmptyCollection
+    }
+    "fail to append empty permissions" in {
+      dummy.append(Set(), 3L).rejected shouldEqual CannotAppendEmptyCollection
+    }
+    "fail to replace with incorrect rev" in {
+      dummy.replace(Set(perm3), 1L).rejected shouldEqual IncorrectRev(1L, 3L)
+    }
+    "fail to replace with empty permissions" in {
+      dummy.replace(Set(), 3L).rejected shouldEqual CannotReplaceWithEmptyCollection
+    }
+    "fail to replace with subset of minimum" in {
+      dummy.replace(Set(read), 3L).rejected shouldEqual CannotReplaceWithEmptyCollection
+    }
+    "replace non minimum" in {
+      dummy.replace(Set(perm3, perm4), 3L).accepted
+      dummy.fetchPermissionSet.accepted shouldEqual (minimum ++ Set(perm3, perm4))
+    }
+    "fail to delete with incorrect rev" in {
+      dummy.delete(2L).rejected shouldEqual IncorrectRev(2L, 4L)
+    }
+    "delete permissions" in {
+      dummy.delete(4L).accepted
+      dummy.fetchPermissionSet.accepted shouldEqual minimum
+    }
+    "fail to delete minimum permissions" in {
+      dummy.delete(5L).rejected shouldEqual CannotDeleteMinimumCollection
+    }
+    "return revision for correct rev" in {
+      dummy.fetchAt(4L).accepted.value shouldEqual (minimum ++ Set(perm3, perm4))
+    }
+    "return none for unknown rev" in {
+      dummy.fetchAt(9999L).rejected shouldEqual RevisionNotFound(9999, 5)
+    }
+
+  }
+
+}
