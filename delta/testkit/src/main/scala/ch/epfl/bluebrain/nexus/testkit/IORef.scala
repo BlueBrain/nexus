f57080d58806ced6cce1598525f809041056dfb8
Bogdan Roman <bogdanromanx@gmail.com>
2020-09-18 12:22:52 +0200

    Implemented IOSemaphore and IORef wrappers (#1630)
    * Implemented IOSemaphore and IORef wrappers
    
    * Addressed feedback

---
+++ delta/testkit/src/main/scala/ch/epfl/bluebrain/nexus/testkit/IORef.scala
@@ -0,0 +1,152 @@
+package ch.epfl.bluebrain.nexus.testkit
+
+import cats.data.State
+import cats.effect.concurrent.Ref
+import monix.bio.{Task, UIO}
+
+// $COVERAGE-OFF$
+/**
+  * A bifunctor ref fixed in IO.
+  */
+trait IORef[A] {
+
+  /**
+    * Obtains the current value.
+    *
+    * Since `Ref` is always guaranteed to have a value, the returned action
+    * completes immediately after being bound.
+    */
+  def get: UIO[A]
+
+  /**
+    * Sets the current value to `a`.
+    *
+    * The returned action completes after the reference has been successfully set.
+    *
+    * Satisfies:
+    *   `r.set(fa) *> r.get == fa`
+    */
+  def set(a: A): UIO[Unit]
+
+  /**
+    * Updates the current value using `f` and returns the value that was updated.
+    *
+    * In case of retries caused by concurrent modifications,
+    * the returned value will be the last one before a successful update.
+    */
+  def getAndUpdate(f: A => A): UIO[A] =
+    modify { a =>
+      (f(a), a)
+    }
+
+  /**
+    * Replaces the current value with `a`, returning the previous value.
+    */
+  def getAndSet(a: A): UIO[A] = getAndUpdate(_ => a)
+
+  /**
+    * Updates the current value using `f`, and returns the updated value.
+    */
+  def updateAndGet(f: A => A): UIO[A] =
+    modify { a =>
+      val newA = f(a)
+      (newA, newA)
+    }
+
+  /**
+    * Obtains a snapshot of the current value, and a setter for updating it.
+    * The setter may noop (in which case `false` is returned) if another concurrent
+    * call to `access` uses its setter first.
+    *
+    * Once it has noop'd or been used once, a setter never succeeds again.
+    *
+    * Satisfies:
+    *   `r.access.map(_._1) == r.get`
+    *   `r.access.flatMap { case (v, setter) => setter(f(v)) } == r.tryUpdate(f).map(_.isDefined)`
+    */
+  def access: UIO[(A, A => UIO[Boolean])]
+
+  /**
+    * Attempts to modify the current value once, returning `false` if another
+    * concurrent modification completes between the time the variable is
+    * read and the time it is set.
+    */
+  def tryUpdate(f: A => A): UIO[Boolean]
+
+  /**
+    * Like `tryUpdate` but allows the update function to return an output value of
+    * type `B`. The returned action completes with `None` if the value is not updated
+    * successfully and `Some(b)` otherwise.
+    */
+  def tryModify[B](f: A => (A, B)): UIO[Option[B]]
+
+  /**
+    * Modifies the current value using the supplied update function. If another modification
+    * occurs between the time the current value is read and subsequently updated, the modification
+    * is retried using the new value. Hence, `f` may be invoked multiple times.
+    *
+    * Satisfies:
+    *   `r.update(_ => a) == r.set(a)`
+    */
+  def update(f: A => A): UIO[Unit]
+
+  /**
+    * Like `tryModify` but does not complete until the update has been successfully made.
+    */
+  def modify[B](f: A => (A, B)): UIO[B]
+
+  /**
+    * Update the value of this ref with a state computation.
+    *
+    * The current value of this ref is used as the initial state and the computed output state
+    * is stored in this ref after computation completes. If a concurrent modification occurs,
+    * `None` is returned.
+    */
+  def tryModifyState[B](state: State[A, B]): UIO[Option[B]]
+
+  /**
+    * Like [[tryModifyState]] but retries the modification until successful.
+    */
+  def modifyState[B](state: State[A, B]): UIO[B]
+}
+
+object IORef {
+
+  /**
+    * Like `apply` but returns the newly allocated ref directly instead of wrapping it in `F.delay`.
+    * This method is considered unsafe because it is not referentially transparent -- it allocates
+    * mutable state.
+    *
+    * @see [[Ref.unsafe]]
+    */
+  final def unsafe[A](a: A): IORef[A] =
+    fromTask(Ref.unsafe(a))
+
+  /**
+    * Creates an asynchronous, concurrent mutable reference initialized to the supplied value.
+    *
+    * @see [[Ref.of]]
+    */
+  final def of[A](a: A): UIO[IORef[A]] =
+    Ref[Task].of(a).hideErrors.map(fromTask)
+
+  /**
+    * Lifts a [[Ref]] into an [[IORef]].
+    */
+  final def fromTask[A](ref: Ref[Task, A]): IORef[A] =
+    new IORef[A] {
+      override def get: UIO[A]                                           = ref.get.hideErrors
+      override def set(a: A): UIO[Unit]                                  = ref.set(a).hideErrors
+      override def access: UIO[(A, A => UIO[Boolean])]                   =
+        ref.access.hideErrors.map {
+          case (a, f) => (a, a1 => f(a1).hideErrors)
+        }
+      override def tryUpdate(f: A => A): UIO[Boolean]                    = ref.tryUpdate(f).hideErrors
+      override def tryModify[B](f: A => (A, B)): UIO[Option[B]]          = ref.tryModify(f).hideErrors
+      override def update(f: A => A): UIO[Unit]                          = ref.update(f).hideErrors
+      override def modify[B](f: A => (A, B)): UIO[B]                     = ref.modify(f).hideErrors
+      override def tryModifyState[B](state: State[A, B]): UIO[Option[B]] = ref.tryModifyState(state).hideErrors
+      override def modifyState[B](state: State[A, B]): UIO[B]            = ref.modifyState(state).hideErrors
+    }
+}
+// $COVERAGE-ON$
---
+++ delta/testkit/src/main/scala/ch/epfl/bluebrain/nexus/testkit/IOSemaphore.scala
@@ -0,0 +1,96 @@
+package ch.epfl.bluebrain.nexus.testkit
+
+import cats.effect.concurrent.Semaphore
+import monix.bio.{IO, Task, UIO}
+
+// $COVERAGE-OFF$
+/**
+  * A bifunctor semaphore fixed in IO.
+  */
+trait IOSemaphore {
+
+  /**
+    * Returns the number of permits currently available. Always non-negative.
+    *
+    * May be out of date the instant after it is retrieved.
+    * Use `[[tryAcquire]]` or `[[tryAcquireN]]` if you wish to attempt an
+    * acquire, returning immediately if the current count is not high enough
+    * to satisfy the request.
+    */
+  def available: UIO[Long]
+
+  /**
+    * Obtains a snapshot of the current count. May be negative.
+    *
+    * Like [[available]] when permits are available but returns the number of permits
+    * callers are waiting for when there are no permits available.
+    */
+  def count: UIO[Long]
+
+  /**
+    * Acquires `n` permits.
+    *
+    * The returned effect semantically blocks until all requested permits are
+    * available. Note that acquires are statisfied in strict FIFO order, so given
+    * `s: Semaphore[F]` with 2 permits available, an `acquireN(3)` will
+    * always be satisfied before a later call to `acquireN(1)`.
+    *
+   * @param n number of permits to acquire - must be >= 0
+    */
+  def acquireN(n: Long): UIO[Unit]
+
+  /** Acquires a single permit. Alias for `[[acquireN]](1)`. */
+  def acquire: UIO[Unit] = acquireN(1)
+
+  /**
+    * Acquires `n` permits now and returns `true`, or returns `false` immediately. Error if `n < 0`.
+    *
+    * @param n number of permits to acquire - must be >= 0
+    */
+  def tryAcquireN(n: Long): UIO[Boolean]
+
+  /** Alias for `[[tryAcquireN]](1)`. */
+  def tryAcquire: UIO[Boolean] = tryAcquireN(1)
+
+  /**
+    * Releases `n` permits, potentially unblocking up to `n` outstanding acquires.
+    *
+    * @param n number of permits to release - must be >= 0
+    */
+  def releaseN(n: Long): UIO[Unit]
+
+  /** Releases a single permit. Alias for `[[releaseN]](1)`. */
+  def release: UIO[Unit] = releaseN(1)
+
+  /**
+    * Returns an effect that acquires a permit, runs the supplied effect, and then releases the permit.
+    */
+  def withPermit[E, A](t: IO[E, A]): IO[E, A]
+
+}
+
+object IOSemaphore {
+
+  /**
+    * Creates a new `Semaphore`, initialized with `n` available permits.
+    *
+    * @see [[Semaphore.apply]]
+    */
+  final def apply(n: Long): UIO[IOSemaphore] =
+    Semaphore[Task](n).hideErrors.map(fromTask)
+
+  /**
+    * Lifts a [[Semaphore]] into an [[IOSemaphore]].
+    */
+  final def fromTask(semaphore: Semaphore[Task]): IOSemaphore =
+    new IOSemaphore {
+      override def available: UIO[Long]                    = semaphore.available.hideErrors
+      override def count: UIO[Long]                        = semaphore.count.hideErrors
+      override def acquireN(n: Long): UIO[Unit]            = semaphore.acquireN(n).hideErrors
+      override def tryAcquireN(n: Long): UIO[Boolean]      = semaphore.tryAcquireN(n).hideErrors
+      override def releaseN(n: Long): UIO[Unit]            = semaphore.releaseN(n).hideErrors
+      override def withPermit[E, A](t: IO[E, A]): IO[E, A] =
+        semaphore.withPermit(t.attempt).hideErrors.flatMap(IO.fromEither)
+    }
+}
+// $COVERAGE-ON$
