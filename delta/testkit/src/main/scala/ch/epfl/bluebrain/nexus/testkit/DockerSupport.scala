6990303745e0ff8f259d2e75b18efa1811b2f57e
Bogdan Roman <bogdan.roman@epfl.ch>
2020-11-04 17:43:10 +0100

    Increased readTimeout for the docker client

--- delta/testkit/src/main/scala/ch/epfl/bluebrain/nexus/testkit/DockerSupport.scala
+++ delta/testkit/src/main/scala/ch/epfl/bluebrain/nexus/testkit/DockerSupport.scala
@@ -1,18 +1,31 @@
 package ch.epfl.bluebrain.nexus.testkit
 
+import com.spotify.docker.client.DefaultDockerClient
+import com.whisk.docker.DockerFactory
+import com.whisk.docker.impl.spotify.{DockerKitSpotify, SpotifyDockerFactory}
 import izumi.distage.docker.Docker
 
 object DockerSupport {
 
-  def clientConfig =
+  def clientConfig: Docker.ClientConfig =
     Docker.ClientConfig(
       readTimeoutMs = 60000, // long timeout for gh actions
-      connectTimeoutMs = 500,
+      connectTimeoutMs = 5000,
       allowReuse = true,
       useRemote = false,
       useRegistry = true,
       remote = None,
       registry = None
     )
+
+  trait DockerKitWithFactory extends DockerKitSpotify {
+    implicit override val dockerFactory: DockerFactory = new SpotifyDockerFactory(
+      DefaultDockerClient
+        .fromEnv()
+        .connectTimeoutMillis(clientConfig.connectTimeoutMs.toLong)
+        .readTimeoutMillis(clientConfig.readTimeoutMs.toLong)
+        .build()
+    )
+  }
 
 }
