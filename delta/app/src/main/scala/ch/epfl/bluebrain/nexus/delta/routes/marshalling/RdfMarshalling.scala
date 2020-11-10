7d52cd6374872a22577b1ab32b9a2fd3e0fa7ba6
Bogdan Roman <bogdanromanx@gmail.com>
2020-11-10 09:37:30 +0100

    Fixed content negotiation for successes and failures (#1770)
    Fixes #1767

--- delta/app/src/main/scala/ch/epfl/bluebrain/nexus/delta/routes/marshalling/RdfMarshalling.scala
+++ delta/app/src/main/scala/ch/epfl/bluebrain/nexus/delta/routes/marshalling/RdfMarshalling.scala
@@ -2,13 +2,14 @@ package ch.epfl.bluebrain.nexus.delta.routes.marshalling
 
 import akka.http.scaladsl.marshalling.{Marshaller, ToEntityMarshaller}
 import akka.http.scaladsl.model.{ContentType, HttpEntity}
+import akka.http.scaladsl.model.ContentTypes.`application/json`
 import akka.util.ByteString
 import ch.epfl.bluebrain.nexus.delta.rdf.graph.{Dot, NTriples}
 import ch.epfl.bluebrain.nexus.delta.rdf.jsonld.JsonLd
 import ch.epfl.bluebrain.nexus.delta.rdf.utils.JsonKeyOrdering
 import ch.epfl.bluebrain.nexus.delta.routes.marshalling.RdfMediaTypes._
 import ch.epfl.bluebrain.nexus.delta.syntax._
-import io.circe.Printer
+import io.circe.{Json, Printer}
 
 /**
   * Marshallings that allow Akka Http to convert a type ''A'' to an [[HttpEntity]].
@@ -32,19 +33,33 @@ trait RdfMarshalling {
     }
 
   /**
+    * Json -> HttpEntity
+    */
+  implicit def jsonMarshaller(implicit
+      ordering: JsonKeyOrdering,
+      printer: Printer = defaultPrinter
+  ): ToEntityMarshaller[Json] =
+    Marshaller.withFixedContentType(`application/json`) { json =>
+      HttpEntity(
+        `application/json`,
+        ByteString(printer.printToByteBuffer(json.sort, `application/json`.charset.nioCharset()))
+      )
+    }
+
+  /**
     * NTriples -> HttpEntity
     */
   implicit val ntriplesMarshaller: ToEntityMarshaller[NTriples] =
     Marshaller.withFixedContentType(ContentType(`application/n-triples`)) { case NTriples(value, _) =>
-      HttpEntity(`application/n-triples`, value)
+      HttpEntity(`application/n-triples`, ByteString(value))
     }
 
   /**
     * Dot -> HttpEntity
     */
   implicit val dotMarshaller: ToEntityMarshaller[Dot] =
-    Marshaller.withFixedContentType(ContentType(`application/vnd.graphviz`)) { case Dot(value, _) =>
-      HttpEntity(`application/vnd.graphviz`, value)
+    Marshaller.withFixedContentType(ContentType(`text/vnd.graphviz`)) { case Dot(value, _) =>
+      HttpEntity(`text/vnd.graphviz`, ByteString(value))
     }
 }
 
