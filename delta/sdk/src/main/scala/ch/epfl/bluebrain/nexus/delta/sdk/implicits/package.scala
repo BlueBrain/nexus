a377b48ee1eb7801fbd87d9cb698276a49329a4b
Didac Montero <didac.monteromendez@epfl.ch>
2021-02-08 12:34:31 +0100

    Added support for handling rejections using DeltaDirectives

--- delta/sdk/src/main/scala/ch/epfl/bluebrain/nexus/delta/sdk/implicits/package.scala
+++ delta/sdk/src/main/scala/ch/epfl/bluebrain/nexus/delta/sdk/implicits/package.scala
@@ -3,7 +3,7 @@ package ch.epfl.bluebrain.nexus.delta.sdk
 import ch.epfl.bluebrain.nexus.delta.kernel.syntax.KamonSyntax
 import ch.epfl.bluebrain.nexus.delta.rdf.instances.{SecretInstances, TripleInstances, UriInstances}
 import ch.epfl.bluebrain.nexus.delta.rdf.syntax.{IriSyntax, IterableSyntax, JsonLdEncoderSyntax, JsonSyntax, PathSyntax, UriSyntax}
-import ch.epfl.bluebrain.nexus.delta.sdk.syntax.{HttpRequestSyntax, HttpResponseFieldsSyntax, IOFunctorSyntax}
+import ch.epfl.bluebrain.nexus.delta.sdk.syntax.{HttpRequestSyntax, HttpResponseFieldsSyntax, IOSyntax}
 
 /**
   * Aggregate instances and syntax from rdf plus the current sdk instances and syntax to avoid importing multiple instances and syntax
@@ -18,7 +18,7 @@ package object implicits
     with PathSyntax
     with IterableSyntax
     with KamonSyntax
-    with IOFunctorSyntax
+    with IOSyntax
     with HttpRequestSyntax
     with HttpResponseFieldsSyntax
     with SecretInstances
