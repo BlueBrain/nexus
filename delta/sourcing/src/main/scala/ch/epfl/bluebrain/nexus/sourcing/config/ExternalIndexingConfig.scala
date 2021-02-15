76bdd7d6476dbdd1fb7d9de146de23a6a29e4c20
Didac Montero <didac.monteromendez@epfl.ch>
2021-02-15 09:45:50 +0100

    Verify prefix with regex

--- delta/sourcing/src/main/scala/ch/epfl/bluebrain/nexus/sourcing/config/ExternalIndexingConfig.scala
+++ delta/sourcing/src/main/scala/ch/epfl/bluebrain/nexus/sourcing/config/ExternalIndexingConfig.scala
@@ -2,9 +2,11 @@ package ch.epfl.bluebrain.nexus.sourcing.config
 
 import ch.epfl.bluebrain.nexus.delta.kernel.RetryStrategyConfig
 import pureconfig.ConfigReader
+import pureconfig.error.FailureReason
 import pureconfig.generic.semiauto.deriveReader
 
 import scala.concurrent.duration.FiniteDuration
+import scala.util.matching.Regex
 
 /**
   * Configuration for indexing into a an external indexer (i.e. elasticsearch)
@@ -24,6 +26,18 @@ final case class ExternalIndexingConfig(
 )
 
 object ExternalIndexingConfig {
+
+  private val prefixRegex: Regex = "[a-zA-Z_-]{1,15}".r
+
+  final case class WrongPrefix(prefix: String) extends FailureReason {
+    val description: String = s"Wrong 'prefix'. '$prefix' did not match the expected format '${prefixRegex.regex}'"
+  }
+
   implicit final val externalIndexingConfigReader: ConfigReader[ExternalIndexingConfig] =
-    deriveReader[ExternalIndexingConfig]
+    deriveReader[ExternalIndexingConfig].emap { cfg =>
+      cfg.prefix match {
+        case prefixRegex() => Right(cfg)
+        case _             => Left(WrongPrefix(cfg.prefix))
+      }
+    }
 }
