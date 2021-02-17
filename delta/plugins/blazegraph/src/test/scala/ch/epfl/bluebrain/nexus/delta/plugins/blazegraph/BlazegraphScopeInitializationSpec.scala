8e41bfae6d0317a86476a1dc97047b46970daf30
Bogdan Roman <bogdanromanx@gmail.com>
2021-02-17 15:52:37 +0100

    Create default SparqlView on project creation (#2047)
    Fixes #1987

---
+++ delta/plugins/blazegraph/src/test/scala/ch/epfl/bluebrain/nexus/delta/plugins/blazegraph/BlazegraphScopeInitializationSpec.scala
@@ -0,0 +1,106 @@
+package ch.epfl.bluebrain.nexus.delta.plugins.blazegraph
+
+import ch.epfl.bluebrain.nexus.delta.kernel.utils.UUIDF
+import ch.epfl.bluebrain.nexus.delta.plugins.blazegraph.model.BlazegraphView.{AggregateBlazegraphView, IndexingBlazegraphView}
+import ch.epfl.bluebrain.nexus.delta.plugins.blazegraph.model.BlazegraphViewRejection.ViewNotFound
+import ch.epfl.bluebrain.nexus.delta.plugins.blazegraph.model._
+import ch.epfl.bluebrain.nexus.delta.rdf.Vocabulary
+import ch.epfl.bluebrain.nexus.delta.rdf.Vocabulary.{nxv, schema => schemaorg}
+import ch.epfl.bluebrain.nexus.delta.rdf.jsonld.context.RemoteContextResolution
+import ch.epfl.bluebrain.nexus.delta.sdk.eventlog.EventLogUtils
+import ch.epfl.bluebrain.nexus.delta.sdk.generators.ProjectGen
+import ch.epfl.bluebrain.nexus.delta.sdk.model.IdSegment.IriSegment
+import ch.epfl.bluebrain.nexus.delta.sdk.model.identities.Identity.{Subject, User}
+import ch.epfl.bluebrain.nexus.delta.sdk.model.identities.ServiceAccount
+import ch.epfl.bluebrain.nexus.delta.sdk.model.projects.ApiMappings
+import ch.epfl.bluebrain.nexus.delta.sdk.model.{BaseUri, Envelope, Label}
+import ch.epfl.bluebrain.nexus.delta.sdk.testkit.{AbstractDBSpec, ConfigFixtures, PermissionsDummy, ProjectSetup}
+import ch.epfl.bluebrain.nexus.sourcing.EventLog
+import ch.epfl.bluebrain.nexus.sourcing.processor.EventSourceProcessorConfig
+import ch.epfl.bluebrain.nexus.testkit.{IOFixedClock, IOValues, TestHelpers}
+import monix.execution.Scheduler
+import org.scalatest.Inspectors
+import org.scalatest.matchers.should.Matchers
+
+import java.util.UUID
+import scala.concurrent.duration.DurationInt
+
+class BlazegraphScopeInitializationSpec
+    extends AbstractDBSpec
+    with Matchers
+    with Inspectors
+    with IOFixedClock
+    with IOValues
+    with TestHelpers
+    with ConfigFixtures {
+
+  private val uuid                   = UUID.randomUUID()
+  implicit private val uuidF: UUIDF  = UUIDF.fixed(uuid)
+  implicit private val sc: Scheduler = Scheduler.global
+
+  private val saRealm: Label              = Label.unsafe("service-accounts")
+  private val usersRealm: Label           = Label.unsafe("users")
+  implicit private val sa: ServiceAccount = ServiceAccount(User("nexus-sa", saRealm))
+  implicit private val bob: Subject       = User("bob", usersRealm)
+
+  private val org      = Label.unsafe("org")
+  private val am       = ApiMappings(Map("nxv" -> nxv.base, "Person" -> schemaorg.Person))
+  private val projBase = nxv.base
+  private val project  =
+    ProjectGen.project("org", "project", uuid = uuid, orgUuid = uuid, base = projBase, mappings = am)
+
+  val views: BlazegraphViews = {
+    implicit val baseUri: BaseUri = BaseUri.withoutPrefix("http://localhost")
+
+    implicit val rcr: RemoteContextResolution = RemoteContextResolution.fixed(
+      Vocabulary.contexts.metadata -> jsonContentOf("/contexts/metadata.json"),
+      contexts.blazegraph          -> jsonContentOf("/contexts/blazegraph.json")
+    )
+
+    val allowedPerms    = Set(defaultPermission)
+    val perms           = PermissionsDummy(allowedPerms).accepted
+    val processorConfig = EventSourceProcessorConfig(3.second, 3.second, system.classicSystem.dispatcher, 10)
+    val config          = BlazegraphViewsConfig(
+      aggregate,
+      keyValueStore,
+      pagination,
+      cacheIndexing,
+      externalIndexing,
+      processorConfig
+    )
+
+    (for {
+      eventLog <- EventLog.postgresEventLog[Envelope[BlazegraphViewEvent]](EventLogUtils.toEnvelope).hideErrors
+      (o, p)   <- ProjectSetup.init(List(org), List(project))
+      views    <- BlazegraphViews(config, eventLog, perms, o, p)
+    } yield views).accepted
+  }
+
+  "A BlazegraphScopeInitialization" should {
+    val init = new BlazegraphScopeInitialization(views, sa)
+
+    "create a default SparqlView on newly created project" in {
+      views.fetch(IriSegment(nxv + "defaultSparqlIndex"), project.ref).rejectedWith[ViewNotFound]
+      init.onProjectCreation(project, bob).accepted
+      val resource = views.fetch(IriSegment(nxv + "defaultSparqlIndex"), project.ref).accepted
+      resource.value match {
+        case v: IndexingBlazegraphView  =>
+          v.resourceSchemas shouldBe empty
+          v.resourceTypes shouldBe empty
+          v.resourceTag shouldEqual None
+          v.includeDeprecated shouldEqual true
+          v.includeMetadata shouldEqual true
+          v.permission shouldEqual defaultPermission
+        case _: AggregateBlazegraphView => fail("Expected an IndexingBlazegraphView to be created")
+      }
+      resource.rev shouldEqual 1L
+      resource.createdBy shouldEqual sa.caller.subject
+    }
+
+    "not create a default SparqlView if one already exists" in {
+      views.fetch(IriSegment(nxv + "defaultSparqlIndex"), project.ref).accepted.rev shouldEqual 1L
+      init.onProjectCreation(project, bob).accepted
+      views.fetch(IriSegment(nxv + "defaultSparqlIndex"), project.ref).accepted.rev shouldEqual 1L
+    }
+  }
+}
