792c152d68205f7ebe84c46750827a16509575fb
Simon Dumas <simon.dumas@epfl.ch>
2021-03-01 16:48:15 +0100

    Scalafmt

--- delta/sdk/src/main/scala/ch/epfl/bluebrain/nexus/delta/sdk/MigrationState.scala
+++ delta/sdk/src/main/scala/ch/epfl/bluebrain/nexus/delta/sdk/MigrationState.scala
@@ -5,6 +5,6 @@ object MigrationState {
 
   def isRunning: Boolean = sys.env.isDefinedAt("MIGRATE_DATA")
 
-  def isIndexingDisabled : Boolean = sys.env.isDefinedAt("DISABLE_INDEXING")
+  def isIndexingDisabled: Boolean = sys.env.isDefinedAt("DISABLE_INDEXING")
 
 }
