7a5a3b4ab3b107bdb2f40433ef78555cde1f7004
Bogdan Roman <bogdan.roman@epfl.ch>
2021-03-29 12:03:55 +0200

    CompositeView module wiring
    App/test is now running in a forked jvm.
    
    Fixes #2105

--- delta/plugins/composite-views/src/test/scala/ch/epfl/bluebrain/nexus/delta/plugins/compositeviews/RemoteContextResolutionFixture.scala
+++ delta/plugins/composite-views/src/test/scala/ch/epfl/bluebrain/nexus/delta/plugins/compositeviews/RemoteContextResolutionFixture.scala
@@ -9,13 +9,14 @@ trait RemoteContextResolutionFixture extends IOValues {
   implicit private val cl: ClassLoader = getClass.getClassLoader
 
   implicit val rcr: RemoteContextResolution = RemoteContextResolution.fixed(
-    contexts.compositeView         -> ContextValue.fromFile("contexts/compositeview.json").accepted,
-    Vocabulary.contexts.metadata   -> ContextValue.fromFile("contexts/metadata.json").accepted,
-    Vocabulary.contexts.error      -> ContextValue.fromFile("contexts/error.json").accepted,
-    Vocabulary.contexts.shacl      -> ContextValue.fromFile("contexts/shacl.json").accepted,
-    Vocabulary.contexts.statistics -> ContextValue.fromFile("/contexts/statistics.json").accepted,
-    Vocabulary.contexts.offset     -> ContextValue.fromFile("/contexts/offset.json").accepted,
-    Vocabulary.contexts.tags       -> ContextValue.fromFile("contexts/tags.json").accepted,
-    Vocabulary.contexts.search     -> ContextValue.fromFile("contexts/search.json").accepted
+    contexts.compositeViews         -> ContextValue.fromFile("contexts/composite-views.json").accepted,
+    contexts.compositeViewsMetadata -> ContextValue.fromFile("contexts/composite-views-metadata.json").accepted,
+    Vocabulary.contexts.metadata    -> ContextValue.fromFile("contexts/metadata.json").accepted,
+    Vocabulary.contexts.error       -> ContextValue.fromFile("contexts/error.json").accepted,
+    Vocabulary.contexts.shacl       -> ContextValue.fromFile("contexts/shacl.json").accepted,
+    Vocabulary.contexts.statistics  -> ContextValue.fromFile("/contexts/statistics.json").accepted,
+    Vocabulary.contexts.offset      -> ContextValue.fromFile("/contexts/offset.json").accepted,
+    Vocabulary.contexts.tags        -> ContextValue.fromFile("contexts/tags.json").accepted,
+    Vocabulary.contexts.search      -> ContextValue.fromFile("contexts/search.json").accepted
   )
 }
