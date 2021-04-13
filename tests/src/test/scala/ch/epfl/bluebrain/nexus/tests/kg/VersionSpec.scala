2f68ffe8572f5fe41552027a18e7fda5cdbc23ec
Bogdan Roman <bogdanromanx@gmail.com>
2021-04-13 13:09:31 +0200

    Fixed AppInfo(Version) IT tests (#2254)

---
+++ tests/src/test/scala/ch/epfl/bluebrain/nexus/tests/kg/VersionSpec.scala
@@ -0,0 +1,70 @@
+package ch.epfl.bluebrain.nexus.tests.kg
+
+import akka.http.scaladsl.model.{StatusCodes, Uri}
+import ch.epfl.bluebrain.nexus.testkit.EitherValuable
+import ch.epfl.bluebrain.nexus.tests.Tags.VersionTag
+import ch.epfl.bluebrain.nexus.tests.iam.types.Permission
+import ch.epfl.bluebrain.nexus.tests.kg.VersionSpec.VersionBundle
+import ch.epfl.bluebrain.nexus.tests.{BaseSpec, Identity}
+import io.circe.generic.semiauto.deriveDecoder
+import io.circe.{Decoder, Json}
+import monix.execution.Scheduler.Implicits.global
+
+class VersionSpec extends BaseSpec with EitherValuable {
+
+  val versionUri: Uri = Uri(s"http://${System.getProperty("delta:8080")}/v1/version")
+
+  "The /version endpoint" should {
+    s"be protected by ${Permission.Version.Read.value}" in {
+      deltaClient.get[Json]("/version", Identity.Anonymous) { (_, response) =>
+        response.status shouldEqual StatusCodes.Forbidden
+      }
+    }
+
+    "return the dependencies and plugin versions" taggedAs VersionTag in {
+      aclDsl.addPermissionAnonymous("/", Permission.Version.Read).runSyncUnsafe()
+
+      deltaClient.get[Json]("/version", Identity.Anonymous) { (json, response) =>
+        response.status shouldEqual StatusCodes.OK
+        json.as[VersionBundle].rightValue
+        succeed
+      }
+    }
+  }
+
+}
+
+object VersionSpec {
+
+  final case class DependenciesBundle(
+      blazegraph: String,
+      cassandra: String,
+      elasticsearch: String,
+      remoteStorage: String
+  )
+  object DependenciesBundle {
+    implicit val dependenciesBundleDecoder: Decoder[DependenciesBundle] = deriveDecoder[DependenciesBundle]
+  }
+
+  final case class PluginsBundle(
+      archive: String,
+      blazegraph: String,
+      elasticsearch: String,
+      `composite-views`: String,
+      storage: String
+  )
+  object PluginsBundle {
+    implicit val pluginsBundleDecoder: Decoder[PluginsBundle] = deriveDecoder[PluginsBundle]
+  }
+
+  final case class VersionBundle(
+      `@context`: String,
+      delta: String,
+      dependencies: DependenciesBundle,
+      plugins: PluginsBundle
+  )
+  object VersionBundle {
+    implicit val versionBundleDecoder: Decoder[VersionBundle] = deriveDecoder[VersionBundle]
+  }
+
+}
