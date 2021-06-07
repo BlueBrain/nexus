7a219cbae5deb72edb7a420767d546772123df3a
Didac Montero <didac.monteromendez@epfl.ch>
2021-06-07 16:22:35 +0200

    Addressed feedback

--- delta/plugins/blazegraph/src/main/scala/ch/epfl/bluebrain/nexus/delta/plugins/blazegraph/indexing/BlazegraphOnEventInstant.scala
+++ delta/plugins/blazegraph/src/main/scala/ch/epfl/bluebrain/nexus/delta/plugins/blazegraph/indexing/BlazegraphOnEventInstant.scala
@@ -2,15 +2,15 @@ package ch.epfl.bluebrain.nexus.delta.plugins.blazegraph.indexing
 
 import ch.epfl.bluebrain.nexus.delta.plugins.blazegraph.BlazegraphViews
 import ch.epfl.bluebrain.nexus.delta.plugins.blazegraph.indexing.BlazegraphIndexingCoordinator.BlazegraphIndexingCoordinator
-import ch.epfl.bluebrain.nexus.delta.plugins.blazegraph.model.{BlazegraphViewSearchParams, BlazegraphViewsConfig}
+import ch.epfl.bluebrain.nexus.delta.plugins.blazegraph.model.{BlazegraphViewSearchParams, BlazegraphViewType, BlazegraphViewsConfig}
 import ch.epfl.bluebrain.nexus.delta.sdk.model.ResourceF
 import ch.epfl.bluebrain.nexus.delta.sdk.model.projects.ProjectRef
 import ch.epfl.bluebrain.nexus.delta.sdk.model.search.Pagination.OnePage
 import ch.epfl.bluebrain.nexus.delta.sdk.syntax._
 import ch.epfl.bluebrain.nexus.delta.sdk.views.indexing.OnEventInstant
 import monix.bio.Task
-import scala.concurrent.duration._
 
+import scala.concurrent.duration._
 import java.time.Instant
 
 final class BlazegraphOnEventInstant(
@@ -24,7 +24,7 @@ final class BlazegraphOnEventInstant(
       prevEvent: Option[Instant],
       currentEvent: Instant
   ): Task[Unit] = {
-    val idleTimeout = config.idleTimeout.minus(config.idleTimeout.div(10).min(1.minute)) // allow for some tolerance
+    val idleTimeout = config.idleTimeout.minus(1.minute) // allow for some tolerance
     Task.when(currentEvent.diff(prevEvent.getOrElse(currentEvent)).gteq(idleTimeout)) {
       val currentProjectViews = views.list(OnePage, searchParams(project), ResourceF.defaultSort).map(_.sources)
       currentProjectViews.flatMap { viewSeq =>
@@ -34,6 +34,10 @@ final class BlazegraphOnEventInstant(
   }
 
   private def searchParams(project: ProjectRef) =
-    BlazegraphViewSearchParams(project = Some(project), deprecated = Some(false), filter = _ => true)
+    BlazegraphViewSearchParams(
+      project = Some(project),
+      deprecated = Some(false),
+      filter = _.tpe == BlazegraphViewType.IndexingBlazegraphView
+    )
 
 }
--- delta/plugins/elasticsearch/src/main/scala/ch/epfl/bluebrain/nexus/delta/plugins/elasticsearch/indexing/ElasticSearchOnEventInstant.scala
+++ delta/plugins/elasticsearch/src/main/scala/ch/epfl/bluebrain/nexus/delta/plugins/elasticsearch/indexing/ElasticSearchOnEventInstant.scala
@@ -3,7 +3,7 @@ package ch.epfl.bluebrain.nexus.delta.plugins.elasticsearch.indexing
 import ch.epfl.bluebrain.nexus.delta.plugins.elasticsearch.ElasticSearchViews
 import ch.epfl.bluebrain.nexus.delta.plugins.elasticsearch.config.ElasticSearchViewsConfig
 import ch.epfl.bluebrain.nexus.delta.plugins.elasticsearch.indexing.ElasticSearchIndexingCoordinator.ElasticSearchIndexingCoordinator
-import ch.epfl.bluebrain.nexus.delta.plugins.elasticsearch.model.ElasticSearchViewSearchParams
+import ch.epfl.bluebrain.nexus.delta.plugins.elasticsearch.model.{ElasticSearchViewSearchParams, ElasticSearchViewType}
 import ch.epfl.bluebrain.nexus.delta.sdk.model.ResourceF
 import ch.epfl.bluebrain.nexus.delta.sdk.model.projects.ProjectRef
 import ch.epfl.bluebrain.nexus.delta.sdk.model.search.Pagination.OnePage
@@ -25,7 +25,7 @@ final class ElasticSearchOnEventInstant(
       prevEvent: Option[Instant],
       currentEvent: Instant
   ): Task[Unit] = {
-    val idleTimeout = config.idleTimeout.minus(config.idleTimeout.div(10).min(1.minute)) // allow for some tolerance
+    val idleTimeout = config.idleTimeout.minus(1.minute) // allow for some tolerance
     Task.when(currentEvent.diff(prevEvent.getOrElse(currentEvent)).gteq(idleTimeout)) {
       val currentProjectViews = views.list(OnePage, searchParams(project), ResourceF.defaultSort).map(_.sources)
       currentProjectViews.flatMap { viewSeq =>
@@ -35,6 +35,10 @@ final class ElasticSearchOnEventInstant(
   }
 
   private def searchParams(project: ProjectRef) =
-    ElasticSearchViewSearchParams(project = Some(project), deprecated = Some(false), filter = _ => true)
+    ElasticSearchViewSearchParams(
+      project = Some(project),
+      deprecated = Some(false),
+      filter = _.tpe == ElasticSearchViewType.ElasticSearch
+    )
 
 }
