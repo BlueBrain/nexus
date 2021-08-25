b2ba1eb14cf09ed507a404d1367c715adb9e137e
Simon <simon.dumas@protonmail.com>
2021-08-25 11:46:08 +0200

    Remove project from stats when deleted (#2725)
    Co-authored-by: Simon Dumas <simon.dumas@epfl.ch>

--- delta/sdk/src/test/scala/ch/epfl/bluebrain/nexus/delta/sdk/ProjectsCountsDummy.scala
+++ delta/sdk/src/test/scala/ch/epfl/bluebrain/nexus/delta/sdk/ProjectsCountsDummy.scala
@@ -4,13 +4,17 @@ import ch.epfl.bluebrain.nexus.delta.sdk.model.projects.ProjectCountsCollection.
 import ch.epfl.bluebrain.nexus.delta.sdk.model.projects.{ProjectCountsCollection, ProjectRef}
 import monix.bio.UIO
 
-class ProjectsCountsDummy(collection: ProjectCountsCollection) extends ProjectsCounts {
-  override def get(): UIO[ProjectCountsCollection] = UIO.pure(collection)
+import scala.collection.mutable.{Map => MutableMap}
+
+class ProjectsCountsDummy(map: MutableMap[ProjectRef, ProjectCount]) extends ProjectsCounts {
+  override def get(): UIO[ProjectCountsCollection] = UIO.pure(ProjectCountsCollection(map.toMap))
 
   override def get(project: ProjectRef): UIO[Option[ProjectCount]] = get().map(_.get(project))
+
+  override def remove(project: ProjectRef): UIO[Unit] = UIO.pure(map.remove(project)).void
 }
 
 object ProjectsCountsDummy {
   final def apply(tuples: (ProjectRef, ProjectCount)*): ProjectsCounts =
-    new ProjectsCountsDummy(ProjectCountsCollection(Map(tuples: _*)))
+    new ProjectsCountsDummy(MutableMap(tuples: _*))
 }
