1291077b43a074fb8a29300ab6bae728edc77774
Didac Montero <didac.montero.mendez@gmail.com>
2021-09-20 18:30:35 +0200

    Fixed issues with tests

--- delta/app/src/test/scala/ch/epfl/bluebrain/nexus/delta/CassandraMainSpec.scala
+++ delta/app/src/test/scala/ch/epfl/bluebrain/nexus/delta/CassandraMainSpec.scala
@@ -1,18 +1,18 @@
 package ch.epfl.bluebrain.nexus.delta
 
-import ch.epfl.bluebrain.nexus.testkit.{ElasticSearchDocker, IOValues}
+import ch.epfl.bluebrain.nexus.testkit.IOValues
 import ch.epfl.bluebrain.nexus.testkit.cassandra.CassandraDocker.CassandraSpec
-import org.scalatest.OptionValues
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpecLike
+import org.scalatest.{DoNotDiscover, OptionValues}
 
+@DoNotDiscover
 class CassandraMainSpec
     extends AnyWordSpecLike
     with Matchers
     with IOValues
     with OptionValues
     with CassandraSpec
-    with ElasticSearchDocker
     with MainBehaviors {
 
   override protected def flavour: String = "cassandra"
