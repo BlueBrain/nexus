845cb505264b5996ed681a534eb89e74a71ecfdd
Oliver <20188437+olivergrabinski@users.noreply.github.com>
2022-10-11 14:16:17 +0200

    Add comment

--- delta/sdk/src/main/scala/ch/epfl/bluebrain/nexus/delta/sdk/utils/NonEmptySetUtils.scala
+++ delta/sdk/src/main/scala/ch/epfl/bluebrain/nexus/delta/sdk/utils/NonEmptySetUtils.scala
@@ -7,6 +7,9 @@ object NonEmptySetUtils {
 
   implicit class NonEmptySetTuple2Ops[A: Order, B: Order](nes: NonEmptySet[(A, B)]) {
 
+    /**
+      * Converts a NonEmptySet of 2-element tuple into a Map
+      */
     def toMap: Map[A, B] = { for { element <- nes } yield element }.toMap
 
   }
