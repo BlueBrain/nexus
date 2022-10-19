370532e352c52a8cc87666bb4f1d19ea0217c2db
Oliver <20188437+olivergrabinski@users.noreply.github.com>
2022-10-19 16:34:59 +0200

    Integrating failed elem persistence into the projection process

--- delta/sourcing-psql/src/main/scala/ch/epfl/bluebrain/nexus/delta/sourcing/stream/Projection.scala
+++ delta/sourcing-psql/src/main/scala/ch/epfl/bluebrain/nexus/delta/sourcing/stream/Projection.scala
@@ -3,6 +3,8 @@ package ch.epfl.bluebrain.nexus.delta.sourcing.stream
 import cats.effect.ExitCase
 import cats.effect.concurrent.Ref
 import ch.epfl.bluebrain.nexus.delta.sourcing.config.BatchConfig
+import ch.epfl.bluebrain.nexus.delta.sourcing.stream.Elem.FailedElem
+import com.typesafe.scalalogging.Logger
 import fs2.concurrent.SignallingRef
 import monix.bio.{Fiber, Task, UIO}
 
@@ -57,35 +59,69 @@ final class Projection private[stream] (
 }
 
 object Projection {
-    def apply(projection: CompiledProjection,
-              fetchProgress: UIO[Option[ProjectionProgress]],
-              saveProgress: ProjectionProgress => UIO[Unit])(implicit batch: BatchConfig): Task[Projection] =
-      for {
-        status    <- Ref[Task].of[ExecutionStatus](ExecutionStatus.Pending)
-        signal   <- SignallingRef[Task, Boolean](false)
-        progress <- fetchProgress.map(_.getOrElse(ProjectionProgress.NoProgress))
-        progressRef <- Ref[Task].of(progress)
-        stream    = projection.streamF
-          .apply(progress.offset)(status)(signal)
-          .interruptWhen(signal)
-          .onFinalizeCaseWeak {
-            case ExitCase.Error(th) => status.update(_.failed(th))
-            case ExitCase.Completed => Task.unit // streams stopped through a signal still finish as Completed
-            case ExitCase.Canceled  => Task.unit // the status is updated by the logic that cancels the stream
-          }.mapAccumulate(progress) {
-          case (acc, msg) if msg.offset.value > progress.offset.value => (acc + msg, msg)
-          case (acc, msg)                                  => (acc, msg)
-        }.groupWithin(batch.maxElements, batch.maxInterval).evalTap { elements =>
-          elements.last.fold(Task.unit) { case (newProgress, _) =>
-            progressRef.set(newProgress) >> saveProgress(newProgress)
-          }
-        }
-          .compile
-          .drain
-        // update status to Running at the beginning and to Completed at the end if it's still running
-        task      = status.update(_ => ExecutionStatus.Running) >> stream >> status.update(s => if (s.isRunning) ExecutionStatus.Completed else s)
-        fiber    <- task.start
-        fiberRef <- Ref[Task].of(fiber)
-      } yield new Projection(projection.metadata.name, status, progressRef, signal, fiberRef)
+
+  private val logger: Logger = Logger[Projection]
+
+  def apply(
+      projection: CompiledProjection,
+      fetchProgress: UIO[Option[ProjectionProgress]],
+      saveProgress: ProjectionProgress => UIO[Unit],
+      saveFailedElems: List[FailedElem] => UIO[Unit]
+  )(implicit batch: BatchConfig): Task[Projection] =
+    for {
+      status      <- Ref[Task].of[ExecutionStatus](ExecutionStatus.Pending)
+      signal      <- SignallingRef[Task, Boolean](false)
+      progress    <- fetchProgress.map(_.getOrElse(ProjectionProgress.NoProgress))
+      progressRef <- Ref[Task].of(progress)
+      stream       = projection.streamF
+                       .apply(progress.offset)(status)(signal)
+                       .interruptWhen(signal)
+                       .onFinalizeCaseWeak {
+                         case ExitCase.Error(th) => status.update(_.failed(th))
+                         case ExitCase.Completed => Task.unit // streams stopped through a signal still finish as Completed
+                         case ExitCase.Canceled  => Task.unit // the status is updated by the logic that cancels the stream
+                       }
+                       .mapAccumulate(progress) {
+                         case (acc, msg) if msg.offset.value > progress.offset.value => (acc + msg, msg)
+                         case (acc, msg)                                             => (acc, msg)
+                       }
+                       .groupWithin(batch.maxElements, batch.maxInterval)
+                       .map { elements =>
+                         elements.foldLeft((Option.empty[ProjectionProgress], List.empty[FailedElem])) {
+                           case ((_, failedElems), (newProgress, elem: FailedElem)) =>
+                             (Some(newProgress), failedElems :+ elem)
+                           case ((_, failedElems), (newProgress, _))                =>
+                             (Some(newProgress), failedElems)
+                         }
+                       }
+                       .evalTap {
+                         case (None, _)                        => UIO.unit
+                         case (Some(newProgress), Nil)         =>
+                           UIO.delay(
+                             logger.debug(
+                               s"[${projection.metadata.name}] Progress: ${newProgress.offset.value}. No failed elems to saved."
+                             )
+                           ) >>
+                             progressRef.set(newProgress) >>
+                             saveProgress(newProgress)
+                         case (Some(newProgress), failedElems) =>
+                           UIO.delay(
+                             logger.debug(
+                               s"[${projection.metadata.name}] Progress: ${newProgress.offset.value}. There are ${failedElems.length} failed elems to save"
+                             )
+                           ) >>
+                             progressRef.set(newProgress) >>
+                             saveProgress(newProgress) >>
+                             saveFailedElems(failedElems)
+                       }
+                       .compile
+                       .drain
+      // update status to Running at the beginning and to Completed at the end if it's still running
+      task         = status.update(_ => ExecutionStatus.Running) >> stream >> status.update(s =>
+                       if (s.isRunning) ExecutionStatus.Completed else s
+                     )
+      fiber       <- task.start
+      fiberRef    <- Ref[Task].of(fiber)
+    } yield new Projection(projection.metadata.name, status, progressRef, signal, fiberRef)
 
 }
