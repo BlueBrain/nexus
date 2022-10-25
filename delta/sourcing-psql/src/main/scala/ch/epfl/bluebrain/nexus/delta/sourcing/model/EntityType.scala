21d2a3ad1564bf2336e0992f9b801277ec08aca3
Oliver <20188437+olivergrabinski@users.noreply.github.com>
2022-10-25 13:23:38 +0200

    Add Encoder for EntityType

--- delta/sourcing-psql/src/main/scala/ch/epfl/bluebrain/nexus/delta/sourcing/model/EntityType.scala
+++ delta/sourcing-psql/src/main/scala/ch/epfl/bluebrain/nexus/delta/sourcing/model/EntityType.scala
@@ -1,6 +1,7 @@
 package ch.epfl.bluebrain.nexus.delta.sourcing.model
 
 import doobie.{Get, Put}
+import io.circe.Encoder
 
 /**
   * Entity type
@@ -12,4 +13,7 @@ final case class EntityType(value: String) extends AnyVal {
 object EntityType {
   implicit val entityTypeGet: Get[EntityType] = Get[String].map(EntityType(_))
   implicit val entityTypePut: Put[EntityType] = Put[String].contramap(_.value)
+
+  implicit val entityTypeEncoder: Encoder[EntityType] =
+    Encoder.encodeString.contramap(_.value)
 }
