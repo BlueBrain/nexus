58a7d7aa3bef40a9323a94e16d32dd3d360f2f19
Oliver <20188437+olivergrabinski@users.noreply.github.com>
2022-12-01 10:36:08 +0100

    Add `MigrationLog` for Realms

--- delta/app/src/main/scala/ch/epfl/bluebrain/nexus/delta/wiring/MigrationModule.scala
+++ delta/app/src/main/scala/ch/epfl/bluebrain/nexus/delta/wiring/MigrationModule.scala
@@ -14,13 +14,15 @@ import ch.epfl.bluebrain.nexus.delta.sdk.permissions.Permissions.labelId
 import ch.epfl.bluebrain.nexus.delta.sdk.permissions.model.{PermissionsCommand, PermissionsEvent, PermissionsRejection, PermissionsState}
 import ch.epfl.bluebrain.nexus.delta.sdk.projects.Projects
 import ch.epfl.bluebrain.nexus.delta.sdk.projects.model.{ProjectCommand, ProjectEvent, ProjectRejection, ProjectState}
+import ch.epfl.bluebrain.nexus.delta.sdk.realms.Realms
+import ch.epfl.bluebrain.nexus.delta.sdk.realms.model.{RealmCommand, RealmEvent, RealmRejection, RealmState}
 import ch.epfl.bluebrain.nexus.delta.sdk.resolvers.ResolverResolution.ResourceResolution
 import ch.epfl.bluebrain.nexus.delta.sdk.resolvers.Resolvers
 import ch.epfl.bluebrain.nexus.delta.sdk.resolvers.model.{ResolverCommand, ResolverEvent, ResolverRejection, ResolverState}
 import ch.epfl.bluebrain.nexus.delta.sdk.resources.Resources
 import ch.epfl.bluebrain.nexus.delta.sdk.resources.model.{ResourceCommand, ResourceEvent, ResourceRejection, ResourceState}
 import ch.epfl.bluebrain.nexus.delta.sdk.schemas.Schemas
-import ch.epfl.bluebrain.nexus.delta.sdk.schemas.model.{Schema, SchemaCommand, SchemaEvent, SchemaRejection, SchemaState}
+import ch.epfl.bluebrain.nexus.delta.sdk.schemas.model._
 import ch.epfl.bluebrain.nexus.delta.sourcing.model.{Label, ProjectRef}
 import izumi.distage.model.definition.ModuleDef
 import monix.bio.{IO, UIO}
@@ -43,6 +45,21 @@ object MigrationModule extends ModuleDef {
     )
   }
 
+  // Realms
+  many[MigrationLog].add { (cfg: AppConfig, xas: Transactors, clock: Clock[UIO]) =>
+    MigrationLog.global[Label, RealmState, RealmCommand, RealmEvent, RealmRejection](
+      Realms.definition(
+        _ => IO.terminate(new IllegalStateException("Realm command evaluation should not happen")),
+        (_, _) => IO.terminate(new IllegalStateException("Realm command evaluation should not happen"))
+      )(clock),
+      e => e.label,
+      identity,
+      (e, _) => e,
+      cfg.realms.eventLog,
+      xas
+    )
+  }
+
   // Organizations
   many[MigrationLog].add { (cfg: AppConfig, xas: Transactors, clock: Clock[UIO], uuidF: UUIDF) =>
     MigrationLog.global[Label, OrganizationState, OrganizationCommand, OrganizationEvent, OrganizationRejection](
@@ -50,7 +67,7 @@ object MigrationModule extends ModuleDef {
       e => e.label,
       identity,
       (e, _) => e,
-      cfg.permissions.eventLog,
+      cfg.organizations.eventLog,
       xas
     )
   }
