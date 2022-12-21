71e638c2e806fd70f4b682d49fd7fa728b29e715
Oliver <20188437+olivergrabinski@users.noreply.github.com>
2022-12-21 14:31:41 +0100

    Load only one pod backup

--- delta/migration/scripts/cassandra-init/init.sh
+++ delta/migration/scripts/cassandra-init/init.sh
@@ -1,4 +1,4 @@
-until cqlsh -f /cassandra-init/cassandra_1_5.ddl && cqlsh -f /cassandra-init/create-materialized-view.ddl && sstableloader --nodes localhost  /cassandra-backups/cassandra-0/delta_1_5/messages && sstableloader --nodes localhost  /cassandra-backups/cassandra-1/delta_1_5/messages && sstableloader --nodes localhost  /cassandra-backups/cassandra-2/delta_1_5/messages; do
+until cqlsh -f /cassandra-init/cassandra_1_5.ddl && cqlsh -f /cassandra-init/create-materialized-view.ddl && sstableloader --nodes localhost  /cassandra-backups/cassandra-0/delta_1_5/messages; do
   echo "cqlsh: Cassandra is unavailable to initialize - will retry later"
   sleep 2
 done &
