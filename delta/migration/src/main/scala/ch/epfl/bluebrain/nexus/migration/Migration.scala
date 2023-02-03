cce0b7d2a39d7a6a9644568a8db870980fcf160a
Oliver <20188437+olivergrabinski@users.noreply.github.com>
2023-02-03 14:02:44 +0100

    Handle `InvalidState` occurences during migration (#3652)

--- delta/migration/src/main/scala/ch/epfl/bluebrain/nexus/migration/Migration.scala
+++ delta/migration/src/main/scala/ch/epfl/bluebrain/nexus/migration/Migration.scala
@@ -8,12 +8,14 @@ import ch.epfl.bluebrain.nexus.delta.kernel.database.Transactors
 import ch.epfl.bluebrain.nexus.delta.rdf.syntax.iriStringContextSyntax
 import ch.epfl.bluebrain.nexus.delta.sdk.acls.Acls
 import ch.epfl.bluebrain.nexus.delta.sdk.migration.{MigrationLog, ToMigrateEvent}
+import ch.epfl.bluebrain.nexus.delta.sourcing.EvaluationError.InvalidState
 import ch.epfl.bluebrain.nexus.delta.sourcing.config.BatchConfig
 import ch.epfl.bluebrain.nexus.delta.sourcing.offset.Offset
 import ch.epfl.bluebrain.nexus.delta.sourcing.stream.Elem.SuccessElem
 import ch.epfl.bluebrain.nexus.delta.sourcing.stream._
 import ch.epfl.bluebrain.nexus.delta.sourcing.implicits._
-import ch.epfl.bluebrain.nexus.migration.Migration.{logger, MigrationProgress}
+import ch.epfl.bluebrain.nexus.delta.sourcing.model.EntityType
+import ch.epfl.bluebrain.nexus.migration.Migration.{processEvent, MigrationProgress}
 import ch.epfl.bluebrain.nexus.migration.config.ReplayConfig
 import ch.epfl.bluebrain.nexus.migration.replay.ReplayEvents
 import com.typesafe.config.{ConfigFactory, ConfigParseOptions}
@@ -40,18 +42,6 @@ final class Migration private (
 ) {
 
   private val logMap = logs.map { log => log.entityType -> log }.toMap
-
-  private val processEvent: ToMigrateEvent => Task[Unit] = event =>
-    logMap.get(event.entityType) match {
-      case Some(migrationLog) =>
-        migrationLog(event).tapError { e =>
-          Task.delay { logger.error(s"[${event.persistenceId}] $e") }
-        }
-      case None               =>
-        val message = s"The logMap has no entry for ${event.entityType}"
-        Task.delay { logger.error(message) } >>
-          Task.raiseError(new NoSuchElementException(message))
-    }
 
   private def saveProgress(progress: MigrationProgress) =
     sql"""INSERT INTO migration_offset (name, akka_offset, processed, discarded, failed, instant)
@@ -87,7 +77,7 @@ final class Migration private (
             val (toIgnore, toProcess) = chunk.partitionEither { e =>
               Either.cond(!Migration.toIgnore(e, blacklisted), e, e)
             }
-            val migrated              = toProcess.traverse(processEvent)
+            val migrated              = toProcess.traverse(processEvent(logMap))
             val ignored               = saveIgnored(toIgnore).transact(xas.write)
             val migrationProgress     =
               MigrationProgress(last.offset, last.instant, toProcess.size.toLong, toIgnore.size.toLong, 0L)
@@ -129,6 +119,26 @@ object Migration {
           failed
         )
       }
+    }
+  }
+
+  def processEvent(logMap: Map[EntityType, MigrationLog]): ToMigrateEvent => Task[Unit] = { event =>
+    logMap.get(event.entityType) match {
+      case Some(migrationLog) =>
+        migrationLog(event)
+          .onErrorRecoverWith { case e: InvalidState[_, _] =>
+            Task.delay {
+              logger.warn(
+                s"Invalid state encountered while processing event: $event. Proceeding with migration. Original message: ${e.getMessage}"
+              )
+            }
+          }
+      case None               =>
+        val message = s"The logMap has no entry for ${event.entityType}"
+        Task.delay {
+          logger.error(message)
+        } >>
+          Task.raiseError(new NoSuchElementException(message))
     }
   }
 
