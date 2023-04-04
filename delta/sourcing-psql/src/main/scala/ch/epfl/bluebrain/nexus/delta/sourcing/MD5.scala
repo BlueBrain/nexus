c7a804f573beb5c84556e331a09948a96477545a
Oliver <20188437+olivergrabinski@users.noreply.github.com>
2023-04-04 10:23:55 +0200

    Fix MD5 since MessageDigest is not thread safe

--- delta/sourcing-psql/src/main/scala/ch/epfl/bluebrain/nexus/delta/sourcing/MD5.scala
+++ delta/sourcing-psql/src/main/scala/ch/epfl/bluebrain/nexus/delta/sourcing/MD5.scala
@@ -2,10 +2,23 @@ package ch.epfl.bluebrain.nexus.delta.sourcing
 
 import java.security.MessageDigest
 
+/**
+  * A class providing an MD5 digest.
+  *
+  * Note that the java MessageDigest is *not* thread safe; the same input can generate different hashes when making
+  * concurrent calls to the same instance
+  */
+class MD5 {
+  val digest: MessageDigest = MessageDigest.getInstance("MD5")
+}
+
 object MD5 {
 
-  private val MD5 = MessageDigest.getInstance("MD5")
-
+  /**
+    * Generate a String MD5 hash of the input
+    * @param input
+    *   The String to hash
+    */
   def hash(input: String): String =
-    MD5.digest(input.getBytes).map("%02x".format(_)).mkString
+    new MD5().digest.digest(input.getBytes).map("%02x".format(_)).mkString
 }
