83a876e961043160f2a52c894d04f93e1e3c0dad
Daniel Bell <daniel.bell@epfl.ch>
2023-06-13 23:51:44 +0200

    increase patience on TarDownloadSpec

--- delta/plugins/archive/src/test/scala/ch/epfl/bluebrain/nexus/delta/plugins/archive/TarDownloadSpec.scala
+++ delta/plugins/archive/src/test/scala/ch/epfl/bluebrain/nexus/delta/plugins/archive/TarDownloadSpec.scala
@@ -2,7 +2,11 @@ package ch.epfl.bluebrain.nexus.delta.plugins.archive
 import ch.epfl.bluebrain.nexus.delta.plugins.archive.model.ArchiveFormat
 import ch.epfl.bluebrain.nexus.delta.sdk.AkkaSource
 
+import scala.concurrent.duration.DurationInt
+
 class TarDownloadSpec extends ArchiveDownloadSpec {
+
+  implicit override def patienceConfig: PatienceConfig = PatienceConfig(3.seconds, 10.millis)
   override def format: ArchiveFormat[_] = ArchiveFormat.Tar
 
   override def sourceToMap(source: AkkaSource): Map[String, String] =
