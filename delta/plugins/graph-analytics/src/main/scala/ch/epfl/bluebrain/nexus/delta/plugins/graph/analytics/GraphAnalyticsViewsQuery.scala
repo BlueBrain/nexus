41a4791042494cccac158a09f305e4276417503c
Oliver <20188437+olivergrabinski@users.noreply.github.com>
2023-08-08 17:26:31 +0200

    Add docstring

--- delta/plugins/graph-analytics/src/main/scala/ch/epfl/bluebrain/nexus/delta/plugins/graph/analytics/GraphAnalyticsViewsQuery.scala
+++ delta/plugins/graph-analytics/src/main/scala/ch/epfl/bluebrain/nexus/delta/plugins/graph/analytics/GraphAnalyticsViewsQuery.scala
@@ -9,10 +9,28 @@ import ch.epfl.bluebrain.nexus.delta.sourcing.model.ProjectRef
 import io.circe.{Json, JsonObject}
 import monix.bio.IO
 
+/** Allows to perform elasticsearch queries on Graph Analytics views */
 trait GraphAnalyticsViewsQuery {
+
+  /**
+    * In a given project, perform the provided elasticsearch query on the projects' Graph Analytics view.
+    * @param projectRef
+    *   project in which to make the query
+    * @param query
+    *   elasticsearch query to perform on the Graph Analytics view
+    * @param qp
+    *   the extra query parameters for the elasticsearch index
+    */
   def query(projectRef: ProjectRef, query: JsonObject, qp: Uri.Query): IO[ElasticSearchViewRejection, Json]
 }
 
+/**
+  * A [[GraphAnalyticsViewsQuery]] implementation that uses the [[ElasticSearchClient]] to query views.
+  * @param prefix
+  *   prefix used in the names of the elasticsearch indices
+  * @param client
+  *   elasticsearch client
+  */
 class GraphAnalyticsViewsQueryImpl(prefix: String, client: ElasticSearchClient) extends GraphAnalyticsViewsQuery {
   override def query(projectRef: ProjectRef, query: JsonObject, qp: Uri.Query): IO[ElasticSearchViewRejection, Json] = {
     val index = GraphAnalytics.index(prefix, projectRef)
