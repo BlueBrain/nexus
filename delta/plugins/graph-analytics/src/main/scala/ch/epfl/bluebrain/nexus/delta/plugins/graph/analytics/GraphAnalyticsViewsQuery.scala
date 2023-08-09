f16eef7e455ac96e7fa39c6afb7e451fa54056af
Oliver <20188437+olivergrabinski@users.noreply.github.com>
2023-08-09 18:11:13 +0200

    Add option to query Graph Analytics views (#4148)

---
+++ delta/plugins/graph-analytics/src/main/scala/ch/epfl/bluebrain/nexus/delta/plugins/graph/analytics/GraphAnalyticsViewsQuery.scala
@@ -0,0 +1,40 @@
+package ch.epfl.bluebrain.nexus.delta.plugins.graph.analytics
+
+import akka.http.scaladsl.model.Uri
+import ch.epfl.bluebrain.nexus.delta.plugins.elasticsearch.client.ElasticSearchClient
+import ch.epfl.bluebrain.nexus.delta.plugins.elasticsearch.model.ElasticSearchViewRejection
+import ch.epfl.bluebrain.nexus.delta.plugins.elasticsearch.model.ElasticSearchViewRejection.WrappedElasticSearchClientError
+import ch.epfl.bluebrain.nexus.delta.sdk.model.search.SortList
+import ch.epfl.bluebrain.nexus.delta.sourcing.model.ProjectRef
+import io.circe.{Json, JsonObject}
+import monix.bio.IO
+
+/** Allows to perform elasticsearch queries on Graph Analytics views */
+trait GraphAnalyticsViewsQuery {
+
+  /**
+    * In a given project, perform the provided elasticsearch query on the projects' Graph Analytics view.
+    * @param projectRef
+    *   project in which to make the query
+    * @param query
+    *   elasticsearch query to perform on the Graph Analytics view
+    * @param qp
+    *   the extra query parameters for the elasticsearch index
+    */
+  def query(projectRef: ProjectRef, query: JsonObject, qp: Uri.Query): IO[ElasticSearchViewRejection, Json]
+}
+
+/**
+  * A [[GraphAnalyticsViewsQuery]] implementation that uses the [[ElasticSearchClient]] to query views.
+  * @param prefix
+  *   prefix used in the names of the elasticsearch indices
+  * @param client
+  *   elasticsearch client
+  */
+class GraphAnalyticsViewsQueryImpl(prefix: String, client: ElasticSearchClient) extends GraphAnalyticsViewsQuery {
+  override def query(projectRef: ProjectRef, query: JsonObject, qp: Uri.Query): IO[ElasticSearchViewRejection, Json] = {
+    val index = GraphAnalytics.index(prefix, projectRef)
+    client.search(query, Set(index.value), qp)(SortList.empty).mapError(WrappedElasticSearchClientError)
+  }
+
+}
--- delta/plugins/graph-analytics/src/main/scala/ch/epfl/bluebrain/nexus/delta/plugins/graph/analytics/package.scala
+++ delta/plugins/graph-analytics/src/main/scala/ch/epfl/bluebrain/nexus/delta/plugins/graph/analytics/package.scala
@@ -2,10 +2,15 @@ package ch.epfl.bluebrain.nexus.delta.plugins.graph
 
 import ch.epfl.bluebrain.nexus.delta.rdf.IriOrBNode.Iri
 import ch.epfl.bluebrain.nexus.delta.rdf.Vocabulary.{contexts => nxvContexts}
+import ch.epfl.bluebrain.nexus.delta.sdk.permissions.model.Permission
 
 package object analytics {
   object contexts {
     val relationships: Iri = nxvContexts + "relationships.json"
     val properties: Iri    = nxvContexts + "properties.json"
+  }
+
+  object permissions {
+    final val query: Permission = Permission.unsafe("views/query")
   }
 }
