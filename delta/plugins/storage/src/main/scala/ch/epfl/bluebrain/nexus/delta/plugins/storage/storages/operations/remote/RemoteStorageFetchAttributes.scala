ffaccb694c13a1c3ce05e6afd5a3f3c697b98a31
Daniel Bell <daniel.bell@epfl.ch>
2023-08-30 11:28:16 +0200

    remove authToken from StorageValue

--- delta/plugins/storage/src/main/scala/ch/epfl/bluebrain/nexus/delta/plugins/storage/storages/operations/remote/RemoteStorageFetchAttributes.scala
+++ delta/plugins/storage/src/main/scala/ch/epfl/bluebrain/nexus/delta/plugins/storage/storages/operations/remote/RemoteStorageFetchAttributes.scala
@@ -3,6 +3,7 @@ package ch.epfl.bluebrain.nexus.delta.plugins.storage.storages.operations.remote
 import akka.actor.ActorSystem
 import akka.http.scaladsl.model.Uri
 import ch.epfl.bluebrain.nexus.delta.plugins.storage.storages.StoragesConfig.StorageTypeConfig
+import ch.epfl.bluebrain.nexus.delta.plugins.storage.storages.StoragesConfig.StorageTypeConfig.remoteStorageAuthToken
 import ch.epfl.bluebrain.nexus.delta.plugins.storage.storages.model.StorageValue.RemoteDiskStorageValue
 import ch.epfl.bluebrain.nexus.delta.plugins.storage.storages.operations.{FetchAttributes, StorageFileRejection}
 import ch.epfl.bluebrain.nexus.delta.plugins.storage.storages.operations.StorageFileRejection.FetchAttributeRejection.WrappedFetchRejection
@@ -16,7 +17,7 @@ class RemoteStorageFetchAttributes(
     value: RemoteDiskStorageValue
 )(implicit config: StorageTypeConfig, httpClient: HttpClient, as: ActorSystem)
     extends FetchAttributes {
-  implicit private val cred: Option[AuthToken] = value.authToken(config)
+  implicit private val cred: Option[AuthToken] = remoteStorageAuthToken(config)
   private val client: RemoteDiskStorageClient  = new RemoteDiskStorageClient(value.endpoint)
 
   def apply(path: Uri.Path): IO[StorageFileRejection.FetchAttributeRejection, RemoteDiskStorageFileAttributes] =
