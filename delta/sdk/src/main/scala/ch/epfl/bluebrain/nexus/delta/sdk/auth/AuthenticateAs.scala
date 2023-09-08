ce103a0d8ba77ba0d7bf1ec410f26adf16ade636
Daniel Bell <daniel.bell@epfl.ch>
2023-09-08 20:09:40 +0200

    use label for realm

--- delta/sdk/src/main/scala/ch/epfl/bluebrain/nexus/delta/sdk/auth/AuthenticateAs.scala
+++ delta/sdk/src/main/scala/ch/epfl/bluebrain/nexus/delta/sdk/auth/AuthenticateAs.scala
@@ -1,11 +1,19 @@
 package ch.epfl.bluebrain.nexus.delta.sdk.auth
 
 import ch.epfl.bluebrain.nexus.delta.kernel.Secret
+import ch.epfl.bluebrain.nexus.delta.sourcing.model.Label
 import pureconfig.ConfigReader
+import pureconfig.error.CannotConvert
 import pureconfig.generic.semiauto.deriveReader
 
-case class AuthenticateAs(user: String, password: Secret[String], realm: String)
+import scala.annotation.nowarn
+
+case class AuthenticateAs(user: String, password: Secret[String], realm: Label)
 
 object AuthenticateAs {
-  implicit val configReader: ConfigReader[AuthenticateAs] = deriveReader[AuthenticateAs]
+  @nowarn("cat=unused")
+  implicit private val labelConfigReader: ConfigReader[Label] = ConfigReader.fromString(str =>
+    Label(str).left.map(e => CannotConvert(str, classOf[Label].getSimpleName, e.getMessage))
+  )
+  implicit val configReader: ConfigReader[AuthenticateAs]     = deriveReader[AuthenticateAs]
 }
