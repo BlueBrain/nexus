21cb49b821f093efa734aff7d4386939ae7d727d
dantb <dantb@hotmail.co.uk>
2023-09-19 14:25:41 +0200

    Update ElasticSearch version to 8.9.2

--- delta/testkit/src/main/scala/ch/epfl/bluebrain/nexus/testkit/elasticsearch/ElasticSearchContainer.scala
+++ delta/testkit/src/main/scala/ch/epfl/bluebrain/nexus/testkit/elasticsearch/ElasticSearchContainer.scala
@@ -27,7 +27,7 @@ class ElasticSearchContainer(password: String)
 }
 
 object ElasticSearchContainer {
-  private val Version = "8.9.1"
+  private val Version = "8.9.2"
 
   val ElasticSearchUser                         = "elastic"
   val ElasticSearchPassword                     = "password"
