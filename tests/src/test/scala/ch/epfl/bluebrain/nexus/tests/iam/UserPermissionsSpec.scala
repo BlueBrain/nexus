1ebcb53be8d676d0e183c1fe9c5b40c94355c754
Daniel Bell <daniel.bell@epfl.ch>
2023-09-25 14:52:04 +0200

    use randomised org and project in test

--- tests/src/test/scala/ch/epfl/bluebrain/nexus/tests/iam/UserPermissionsSpec.scala
+++ tests/src/test/scala/ch/epfl/bluebrain/nexus/tests/iam/UserPermissionsSpec.scala
@@ -8,19 +8,21 @@ import ch.epfl.bluebrain.nexus.tests.iam.types.Permission.Resources
 
 class UserPermissionsSpec extends BaseSpec {
 
+  val org, project = genId()
+
   private def urlFor(permission: String, project: String) =
     s"/user/permissions/$project?permission=${encode(permission)}"
 
   "if a user does not have a permission, 403 should be returned" in {
-    deltaClient.head(urlFor("resources/read", "org/project"), UserWithNoPermissions) { response =>
+    deltaClient.head(urlFor("resources/read", s"$org/$project"), UserWithNoPermissions) { response =>
       response.status shouldBe StatusCodes.Forbidden
     }
   }
 
   "if a user has a permission, 204 should be returned" in {
     for {
-      _ <- aclDsl.addPermission("/org/project", UserWithPermissions, Resources.Read)
-      _ <- deltaClient.head(urlFor("resources/read", "org/project"), UserWithPermissions) { response =>
+      _ <- aclDsl.addPermission(s"/$org/$project", UserWithPermissions, Resources.Read)
+      _ <- deltaClient.head(urlFor("resources/read", s"$org/$project"), UserWithPermissions) { response =>
              response.status shouldBe StatusCodes.NoContent
            }
     } yield succeed
