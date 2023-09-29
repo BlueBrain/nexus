c368203d76f1cc30396ad936fa89ce01ce19bd4b
Daniel Bell <daniel.bell@epfl.ch>
2023-09-29 23:12:00 +0200

    scalafmt

--- delta/plugins/storage/src/main/scala/ch/epfl/bluebrain/nexus/delta/plugins/storage/storages/StoragePermissionProviderImpl.scala
+++ delta/plugins/storage/src/main/scala/ch/epfl/bluebrain/nexus/delta/plugins/storage/storages/StoragePermissionProviderImpl.scala
@@ -8,13 +8,17 @@ import ch.epfl.bluebrain.nexus.delta.sourcing.model.ProjectRef
 import monix.bio.UIO
 
 class StoragePermissionProviderImpl(storages: Storages) extends StoragePermissionProvider {
-  override def permissionFor(id: IdSegmentRef, project: ProjectRef, accessType: StoragePermissionProvider.AccessType): UIO[Permission] = {
+  override def permissionFor(
+      id: IdSegmentRef,
+      project: ProjectRef,
+      accessType: StoragePermissionProvider.AccessType
+  ): UIO[Permission] = {
     storages
       .fetch(id, project)
       .map(storage => storage.value.storageValue)
       .map(storage =>
         accessType match {
-          case Read => storage.readPermission
+          case Read  => storage.readPermission
           case Write => storage.writePermission
         }
       )
