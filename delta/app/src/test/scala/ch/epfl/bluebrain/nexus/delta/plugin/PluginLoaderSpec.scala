a1cc3f3db19eee0ed948457bf9dec061f34748a8
Simon Dumas <simon.dumas@epfl.ch>
2023-10-25 16:55:18 +0200

    Migrate app config loading to Cats Effect

--- delta/app/src/test/scala/ch/epfl/bluebrain/nexus/delta/plugin/PluginLoaderSpec.scala
+++ delta/app/src/test/scala/ch/epfl/bluebrain/nexus/delta/plugin/PluginLoaderSpec.scala
@@ -1,42 +1,35 @@
 package ch.epfl.bluebrain.nexus.delta.plugin
 
 import akka.http.scaladsl.testkit.ScalatestRouteTest
-import ch.epfl.bluebrain.nexus.delta.kernel.effect.migration._
+import cats.effect.IO
 import ch.epfl.bluebrain.nexus.delta.plugin.PluginsLoader.PluginLoaderConfig
 import ch.epfl.bluebrain.nexus.delta.sdk.PriorityRoute
 import ch.epfl.bluebrain.nexus.delta.sdk.model.BaseUri
 import ch.epfl.bluebrain.nexus.testkit.ce.CatsRunContext
-import ch.epfl.bluebrain.nexus.testkit.scalatest.bio.BioSpec
+import ch.epfl.bluebrain.nexus.testkit.scalatest.ce.CatsEffectSpec
 import com.typesafe.config.impl.ConfigImpl
 import izumi.distage.model.definition.ModuleDef
-import monix.bio.Task
-import monix.execution.Scheduler
-import monix.execution.Scheduler.Implicits.global
 
-class PluginLoaderSpec extends BioSpec with ScalatestRouteTest with CatsRunContext {
+class PluginLoaderSpec extends CatsEffectSpec with ScalatestRouteTest with CatsRunContext {
 
   private val baseUri       = BaseUri.withoutPrefix("http://localhost")
   private val serviceModule = new ModuleDef {
     make[BaseUri].fromValue(baseUri)
-    make[Scheduler].from(Scheduler.global)
   }
 
   "A PluginLoader" should {
     val config = PluginLoaderConfig("../plugins/test-plugin/target")
     "load plugins from .jar in a directory" in {
       val (_, pluginsDef) = PluginsLoader(config).load.accepted
-      WiringInitializer(serviceModule, pluginsDef)
-        .mapK(ioToTaskK)
-        .use { case (_, locator) =>
-          Task.delay {
-            val route = locator.get[Set[PriorityRoute]].head
-            pluginsDef.head.priority shouldEqual 10
-            Get("/test-plugin") ~> route.route ~> check {
-              responseAs[String] shouldEqual "http://localhost"
-            }
+      WiringInitializer(serviceModule, pluginsDef).use { case (_, locator) =>
+        IO.delay {
+          val route = locator.get[Set[PriorityRoute]].head
+          pluginsDef.head.priority shouldEqual 10
+          Get("/test-plugin") ~> route.route ~> check {
+            responseAs[String] shouldEqual "http://localhost"
           }
         }
-        .accepted
+      }.accepted
     }
 
     "load overriding priority" in {
