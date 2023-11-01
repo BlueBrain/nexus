4cdd09b3ae9dca6871edf76c40d8e0318f43d3d1
dantb <dantb@hotmail.co.uk>
2023-11-01 12:06:19 +0100

    Fix event metrics deletion suite

--- delta/plugins/elasticsearch/src/test/scala/ch/epfl/bluebrain/nexus/delta/plugins/elasticsearch/deletion/EventMetricsDeletionTaskSuite.scala
+++ delta/plugins/elasticsearch/src/test/scala/ch/epfl/bluebrain/nexus/delta/plugins/elasticsearch/deletion/EventMetricsDeletionTaskSuite.scala
@@ -45,7 +45,7 @@ class EventMetricsDeletionTaskSuite
 
     for {
       // Indexing and checking count
-      _ <- client.createIndex(index, Some(defaultMapping), Some(defaultSettings))
+      _ <- client.createIndex(index, Some(metricsMapping), Some(metricsSettings))
       _ <- client.bulk(operations)
       _ <- client.refresh(index)
       _ <- client.count(index.value).assert(4L)
