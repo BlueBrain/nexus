8cbf39b3a59f8f50fd82d7c0fa3fd64007132b36
Oliver <20188437+olivergrabinski@users.noreply.github.com>
2023-12-04 09:57:08 +0100

    Add docstrings

--- delta/plugins/blazegraph/src/main/scala/ch/epfl/bluebrain/nexus/delta/plugins/blazegraph/BlazegraphErrorParser.scala
+++ delta/plugins/blazegraph/src/main/scala/ch/epfl/bluebrain/nexus/delta/plugins/blazegraph/BlazegraphErrorParser.scala
@@ -6,6 +6,9 @@ import ch.epfl.bluebrain.nexus.delta.sdk.http.HttpClientError
 
 object BlazegraphErrorParser {
 
+  /**
+    * Attempts to extract the malformed query message from the raw exception
+    */
   private def parseMalformedQueryException(rawError: String): Option[String] = {
     val javaExecutionExceptionMatcher = "java.util.concurrent.ExecutionException: "
     val malformedSegmentMatcher       = "java.util.concurrent.ExecutionException: org.openrdf.query.MalformedQueryException: "
@@ -15,6 +18,10 @@ object BlazegraphErrorParser {
       .map(str => str.replace(javaExecutionExceptionMatcher, ""))
   }
 
+  /**
+    * Attempts to parse the raw error message. If it cannot be parsed, or if there are multiple errors, the raw error is
+    * returned.
+    */
   private def parse(rawError: String): String = {
     val errors = List(parseMalformedQueryException(rawError)).flatten
     errors match {
@@ -23,6 +30,9 @@ object BlazegraphErrorParser {
     }
   }
 
+  /**
+    * Extract the details from the error
+    */
   def details(error: SparqlClientError): String =
     error match {
       case WrappedHttpClientError(httpError) =>
