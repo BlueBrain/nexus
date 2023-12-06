a769ce030dffe978a13321cb36d664564276c494
Oliver <20188437+olivergrabinski@users.noreply.github.com>
2023-12-06 18:00:07 +0100

    Kill Bob (unused)

--- delta/plugins/blazegraph/src/test/scala/ch/epfl/bluebrain/nexus/delta/plugins/blazegraph/routes/BlazegraphViewRoutesFixtures.scala
+++ delta/plugins/blazegraph/src/test/scala/ch/epfl/bluebrain/nexus/delta/plugins/blazegraph/routes/BlazegraphViewRoutesFixtures.scala
@@ -60,8 +60,6 @@ trait BlazegraphViewRoutesFixtures
   val aclCheck = AclSimpleCheck().accepted
 
   val realm                   = Label.unsafe("myrealm")
-  val bob                     = User("Bob", realm)
-  implicit val caller: Caller = Caller(bob, Set(bob, Group("mygroup", realm), Authenticated(realm)))
 
   val reader = User("reader", realm)
   val writer = User("writer", realm)
