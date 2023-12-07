c19b95cecab47552787f5e087f031f2ee64c06bd
Simon Dumas <simon.dumas@epfl.ch>
2023-12-07 13:16:58 +0100

    Remove printlns

--- delta/plugins/test-plugin/src/main/scala/ch/epfl/bluebrain/nexus/delta/testplugin/TestPlugin.scala
+++ delta/plugins/test-plugin/src/main/scala/ch/epfl/bluebrain/nexus/delta/testplugin/TestPlugin.scala
@@ -9,5 +9,5 @@ import scala.annotation.nowarn
 @nowarn("cat=unused")
 @SuppressWarnings(Array("UnusedMethodParameter"))
 class TestPlugin(baseUri: BaseUri) extends Plugin {
-  override def stop(): IO[Unit] = IO.pure(println(s"Stopping plugin"))
+  override def stop(): IO[Unit] = IO.println(s"Stopping plugin")
 }
