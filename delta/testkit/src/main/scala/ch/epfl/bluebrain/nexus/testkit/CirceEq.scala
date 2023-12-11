9db14807482d121d24d9998b638f6327c3cdb415
Cristina E. González-Espinoza <crisbeth46@gmail.com>
2023-12-11 16:31:56 +0100

    Add query, files and tests for morphology features (#4575)
    Co-authored-by: Oliver <20188437+olivergrabinski@users.noreply.github.com>

--- delta/testkit/src/main/scala/ch/epfl/bluebrain/nexus/testkit/CirceEq.scala
+++ delta/testkit/src/main/scala/ch/epfl/bluebrain/nexus/testkit/CirceEq.scala
@@ -1,9 +1,11 @@
 package ch.epfl.bluebrain.nexus.testkit
 
-import ch.epfl.bluebrain.nexus.testkit.CirceEq.IgnoredArrayOrder
+import cats.Eq
+import cats.implicits.catsSyntaxEq
+import ch.epfl.bluebrain.nexus.testkit.CirceEq.{ignoreJsonKeyOrderEq, IgnoredArrayOrder}
 import io.circe._
 import io.circe.syntax._
-import org.scalatest.matchers.{HavePropertyMatchResult, HavePropertyMatcher, MatchResult, Matcher}
+import org.scalatest.matchers._
 
 trait CirceEq {
   def equalIgnoreArrayOrder(json: Json): IgnoredArrayOrder = IgnoredArrayOrder(json)
@@ -17,24 +19,45 @@ trait CirceEq {
       actualValue
     )
   })
+
+  def arrayThatContains(expectedValue: Json): BeMatcher[Json] = (left: Json) => {
+    implicit val jsonEq: Eq[Json] = ignoreJsonKeyOrderEq
+    MatchResult(
+      left.asArray.exists(arr => arr.exists(_ === expectedValue)),
+      s"Json $left was not an array containing $expectedValue",
+      s"Json $left was an array containing $expectedValue"
+    )
+  }
 }
 
 object CirceEq {
+
+  val ignoreJsonKeyOrderEq: Eq[Json] = new Eq[Json] {
+    implicit private val printer: Printer = Printer.spaces2.copy(dropNullValues = true)
+
+    override def eqv(left: Json, right: Json): Boolean = {
+      val leftSorted  = sortKeys(left)
+      val rightSorted = sortKeys(right)
+      leftSorted == rightSorted || printer.print(leftSorted) == printer.print(rightSorted)
+    }
+  }
+
+  private def sortKeys(value: Json): Json = {
+    def canonicalJson(json: Json): Json =
+      json.arrayOrObject[Json](
+        json,
+        arr => Json.fromValues(arr.map(canonicalJson).sortBy(_.hashCode)),
+        obj => sorted(obj).asJson
+      )
+
+    def sorted(jObj: JsonObject): JsonObject =
+      JsonObject.fromIterable(jObj.toVector.sortBy(_._1).map { case (k, v) => k -> canonicalJson(v) })
+
+    canonicalJson(value)
+  }
+
   final case class IgnoredArrayOrder(json: Json) extends Matcher[Json] {
     implicit private val printer: Printer = Printer.spaces2.copy(dropNullValues = true)
-    private def sortKeys(value: Json): Json = {
-      def canonicalJson(json: Json): Json =
-        json.arrayOrObject[Json](
-          json,
-          arr => Json.fromValues(arr.map(canonicalJson).sortBy(_.hashCode)),
-          obj => sorted(obj).asJson
-        )
-
-      def sorted(jObj: JsonObject): JsonObject =
-        JsonObject.fromIterable(jObj.toVector.sortBy(_._1).map { case (k, v) => k -> canonicalJson(v) })
-
-      canonicalJson(value)
-    }
 
     override def apply(left: Json): MatchResult = {
       val leftSorted  = sortKeys(left)
