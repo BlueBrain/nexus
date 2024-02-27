401822c3bf963df29808d18e0c5011ed09270d33
Oliver <20188437+olivergrabinski@users.noreply.github.com>
2024-02-27 15:06:02 +0100

    Update file creation examples

--- docs/src/main/paradox/docs/delta/api/assets/files/create-put.sh
+++ docs/src/main/paradox/docs/delta/api/assets/files/create-put.sh
@@ -1,4 +1,4 @@
 curl -X PUT \
      -F "file=@/path/to/myfile.pdf;type=application/pdf" \
-     -F 'metadata="{\"name\": \"descriptive name\", \"description\": \"a description\"}"'\
+     -F 'metadata="{\"name\": \"My File\"}"' \
      "http://localhost:8080/v1/files/myorg/myproject/myfile?storage=remote&tag=mytag"
