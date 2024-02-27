b42ac062ac7303d3d6fc8233cdf8d5a7f32cde40
Oliver <20188437+olivergrabinski@users.noreply.github.com>
2024-02-27 14:49:26 +0100

    Update linking doc

--- docs/src/main/paradox/docs/delta/api/assets/files/create-put.sh
+++ docs/src/main/paradox/docs/delta/api/assets/files/create-put.sh
@@ -1,3 +1,4 @@
 curl -X PUT \
      -F "file=@/path/to/myfile.pdf;type=application/pdf" \
+     -F 'metadata="{\"name\": \"descriptive name\", \"description\": \"a description\"}"'\
      "http://localhost:8080/v1/files/myorg/myproject/myfile?storage=remote&tag=mytag"
--- docs/src/main/paradox/docs/delta/api/assets/files/link-post.sh
+++ docs/src/main/paradox/docs/delta/api/assets/files/link-post.sh
@@ -4,5 +4,13 @@ curl -X POST \
    '{
       "path": "relative/path/to/myfile.png",
       "filename": "myfile.png",
-      "mediaType": "image/png"
+      "mediaType": "image/png",
+      "metadata": {
+        "name": "My File",
+        "description": "a description of the file",
+        "keywords": {
+          "key1": "value1",
+          "key2": "value2"
+        }
+      }
    }'
