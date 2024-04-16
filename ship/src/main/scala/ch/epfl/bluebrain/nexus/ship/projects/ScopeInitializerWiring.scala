323d7a0b270d293dab1352929b7ecdeb1993b0a1
Oliver <20188437+olivergrabinski@users.noreply.github.com>
2024-04-16 10:42:16 +0200

    Clean-up storage wiring

--- ship/src/main/scala/ch/epfl/bluebrain/nexus/ship/projects/ScopeInitializerWiring.scala
+++ ship/src/main/scala/ch/epfl/bluebrain/nexus/ship/projects/ScopeInitializerWiring.scala
@@ -2,11 +2,8 @@ package ch.epfl.bluebrain.nexus.ship.projects
 
 import cats.effect.IO
 import ch.epfl.bluebrain.nexus.delta.kernel.utils.UUIDF
-import ch.epfl.bluebrain.nexus.delta.plugins.storage.StorageScopeInitialization
-import ch.epfl.bluebrain.nexus.delta.plugins.storage.storages.StoragesConfig.S3StorageConfig
-import ch.epfl.bluebrain.nexus.delta.plugins.storage.storages.model.StorageFields.S3StorageFields
 import ch.epfl.bluebrain.nexus.delta.rdf.jsonld.api.JsonLdApi
-import ch.epfl.bluebrain.nexus.delta.sdk.{Defaults, ScopeInitializer}
+import ch.epfl.bluebrain.nexus.delta.sdk.ScopeInitializer
 import ch.epfl.bluebrain.nexus.delta.sdk.projects.{FetchContext, ScopeInitializationErrorStore}
 import ch.epfl.bluebrain.nexus.delta.sdk.resolvers.ResolverContextResolution
 import ch.epfl.bluebrain.nexus.delta.sourcing.Transactors
@@ -14,6 +11,7 @@ import ch.epfl.bluebrain.nexus.ship.EventClock
 import ch.epfl.bluebrain.nexus.ship.config.ShipConfig
 import ch.epfl.bluebrain.nexus.ship.storages.StorageWiring
 import ch.epfl.bluebrain.nexus.ship.views.ViewWiring.{blazegraphViews, elasticSearchViews, viewInitializers}
+import ch.epfl.bluebrain.nexus.ship.storages.StorageWiring.s3StorageInitializer
 
 object ScopeInitializerWiring {
 
@@ -25,32 +23,12 @@ object ScopeInitializerWiring {
       xas: Transactors
   )(implicit jsonLdApi: JsonLdApi): IO[ScopeInitializer] =
     for {
-      esViews    <- elasticSearchViews(fetchContext, rcr, config.eventLog, clock, UUIDF.random, xas)
-      bgViews    <- blazegraphViews(fetchContext, rcr, config.eventLog, clock, UUIDF.random, xas)
-      storages   <- StorageWiring.storages(fetchContext, rcr, config, clock, xas)
-      amzConfig  <- IO.fromOption(config.S3.storages.storageTypeConfig.amazon)(
-                      new IllegalArgumentException("Amazon S3 configuration is missing")
-                    )
-      storageInit = StorageScopeInitialization.s3(storages, config.serviceAccount.value, defaultS3Fields(amzConfig))
-      allInits    = viewInitializers(esViews, bgViews, config) + storageInit
-      errorStore  = ScopeInitializationErrorStore(xas, clock)
+      esViews     <- elasticSearchViews(fetchContext, rcr, config.eventLog, clock, UUIDF.random, xas)
+      bgViews     <- blazegraphViews(fetchContext, rcr, config.eventLog, clock, UUIDF.random, xas)
+      storages    <- StorageWiring.storages(fetchContext, rcr, config, clock, xas)
+      storageInit <- s3StorageInitializer(storages, config)
+      allInits     = viewInitializers(esViews, bgViews, config) + storageInit
+      errorStore   = ScopeInitializationErrorStore(xas, clock)
     } yield ScopeInitializer(allInits, errorStore)
-
-  private def defaultS3Fields(config: S3StorageConfig) = {
-    val defaults = Defaults(
-      "S3 storage",
-      "Default S3 storage of the Nexus service"
-    )
-
-    S3StorageFields(
-      name = Some(defaults.name),
-      description = Some(defaults.description),
-      default = true,
-      bucket = "", // TODO correct bucket
-      readPermission = Some(config.defaultReadPermission),
-      writePermission = Some(config.defaultWritePermission),
-      maxFileSize = Some(config.defaultMaxFileSize)
-    )
-  }
 
 }
--- ship/src/main/scala/ch/epfl/bluebrain/nexus/ship/storages/StorageWiring.scala
+++ ship/src/main/scala/ch/epfl/bluebrain/nexus/ship/storages/StorageWiring.scala
@@ -2,15 +2,20 @@ package ch.epfl.bluebrain.nexus.ship.storages
 
 import cats.effect.IO
 import ch.epfl.bluebrain.nexus.delta.kernel.utils.UUIDF
+import ch.epfl.bluebrain.nexus.delta.plugins.storage.StorageScopeInitialization
 import ch.epfl.bluebrain.nexus.delta.plugins.storage.storages.Storages
+import ch.epfl.bluebrain.nexus.delta.plugins.storage.storages.StoragesConfig.S3StorageConfig
+import ch.epfl.bluebrain.nexus.delta.plugins.storage.storages.model.StorageFields.S3StorageFields
 import ch.epfl.bluebrain.nexus.delta.plugins.storage.storages.model.StorageValue
 import ch.epfl.bluebrain.nexus.delta.plugins.storage.storages.operations.StorageAccess
 import ch.epfl.bluebrain.nexus.delta.rdf.jsonld.api.JsonLdApi
+import ch.epfl.bluebrain.nexus.delta.sdk.Defaults
 import ch.epfl.bluebrain.nexus.delta.sdk.projects.FetchContext
 import ch.epfl.bluebrain.nexus.delta.sdk.resolvers.ResolverContextResolution
 import ch.epfl.bluebrain.nexus.delta.sourcing.Transactors
 import ch.epfl.bluebrain.nexus.ship.EventClock
 import ch.epfl.bluebrain.nexus.ship.config.ShipConfig
+import fs2.aws.s3.models.Models.BucketName
 
 object StorageWiring {
 
@@ -20,7 +25,7 @@ object StorageWiring {
       config: ShipConfig,
       clock: EventClock,
       xas: Transactors
-  )(implicit api: JsonLdApi) = {
+  )(implicit api: JsonLdApi): IO[Storages] = {
     val noopAccess   = new StorageAccess {
       override def apply(storage: StorageValue): IO[Unit] = IO.unit
     }
@@ -37,6 +42,37 @@ object StorageWiring {
       config.serviceAccount.value,
       clock
     )(api, UUIDF.random)
+  }
+
+  def s3StorageInitializer(
+      storages: Storages,
+      config: ShipConfig
+  ): IO[StorageScopeInitialization] =
+    IO.fromOption(config.S3.storages.storageTypeConfig.amazon)(
+      new IllegalArgumentException("Amazon S3 configuration is missing")
+    ).map { amzConfig =>
+      StorageScopeInitialization.s3(
+        storages,
+        config.serviceAccount.value,
+        defaultS3Fields(config.S3.defaultBucket, amzConfig)
+      )
+    }
+
+  private def defaultS3Fields(defaultBucket: BucketName, config: S3StorageConfig) = {
+    val defaults = Defaults(
+      "S3 storage",
+      "Default S3 storage of the Nexus service"
+    )
+
+    S3StorageFields(
+      name = Some(defaults.name),
+      description = Some(defaults.description),
+      default = true,
+      bucket = defaultBucket.value.value,
+      readPermission = Some(config.defaultReadPermission),
+      writePermission = Some(config.defaultWritePermission),
+      maxFileSize = Some(config.defaultMaxFileSize)
+    )
   }
 
 }
