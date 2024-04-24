6a7dd5e32cb2195a57835d8a89122c4a0947b459
Oliver <20188437+olivergrabinski@users.noreply.github.com>
2024-04-24 13:15:56 +0200

    Fix config loading

--- ship/src/main/scala/ch/epfl/bluebrain/nexus/ship/InitShip.scala
+++ ship/src/main/scala/ch/epfl/bluebrain/nexus/ship/InitShip.scala
@@ -10,22 +10,21 @@ object InitShip {
 
   def configAndStream(
       run: RunCommand,
+      defaultConfig: ShipConfig,
       s3Client: S3StorageClient
   ): IO[(ShipConfig, fs2.Stream[IO, RowEvent])] = {
-    ShipConfig.load(run.config).flatMap { shipConfig =>
-      run.mode match {
-        case RunMode.Local =>
-          val eventsStream = EventStreamer.localStreamer.stream(run.path, run.offset)
-          ShipConfig.load(run.config).map((_, eventsStream))
-        case RunMode.S3    =>
-          val eventsStream =
-            EventStreamer.s3eventStreamer(s3Client, shipConfig.s3.importBucket).stream(run.path, run.offset)
-          val config       = run.config match {
-            case Some(configPath) => ShipConfig.loadFromS3(s3Client, shipConfig.s3.importBucket, configPath)
-            case None             => IO.pure(shipConfig)
-          }
-          config.map((_, eventsStream))
-      }
+    run.mode match {
+      case RunMode.Local =>
+        val eventsStream = EventStreamer.localStreamer.stream(run.path, run.offset)
+        ShipConfig.load(run.config).map((_, eventsStream))
+      case RunMode.S3    =>
+        val eventsStream =
+          EventStreamer.s3eventStreamer(s3Client, defaultConfig.s3.importBucket).stream(run.path, run.offset)
+        val config       = run.config match {
+          case Some(configPath) => ShipConfig.loadFromS3(s3Client, defaultConfig.s3.importBucket, configPath)
+          case None             => IO.pure(defaultConfig)
+        }
+        config.map((_, eventsStream))
     }
   }
 }
