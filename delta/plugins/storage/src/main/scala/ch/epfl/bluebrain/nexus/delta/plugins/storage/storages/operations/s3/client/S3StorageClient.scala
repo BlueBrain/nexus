d56147890dd09302a37a6eb17f12838f469376ae
dantb <dantb@hotmail.co.uk>
2024-04-29 18:13:34 +0200

    Read events file from S3 using multipart request

--- delta/plugins/storage/src/main/scala/ch/epfl/bluebrain/nexus/delta/plugins/storage/storages/operations/s3/client/S3StorageClient.scala
+++ delta/plugins/storage/src/main/scala/ch/epfl/bluebrain/nexus/delta/plugins/storage/storages/operations/s3/client/S3StorageClient.scala
@@ -9,8 +9,9 @@ import ch.epfl.bluebrain.nexus.delta.plugins.storage.storages.model.StorageRejec
 import ch.epfl.bluebrain.nexus.delta.plugins.storage.storages.operations.s3.client.S3StorageClient.{HeadObject, UploadMetadata}
 import ch.epfl.bluebrain.nexus.delta.rdf.syntax.uriSyntax
 import ch.epfl.bluebrain.nexus.delta.sdk.error.ServiceError.FeatureDisabled
+import eu.timepit.refined.refineMV
 import fs2.aws.s3.S3
-import fs2.aws.s3.models.Models.{BucketName, FileKey}
+import fs2.aws.s3.models.Models.{BucketName, FileKey, PartSizeMB}
 import fs2.{Chunk, Pipe, Stream}
 import io.laserdisc.pure.s3.tagless.{Interpreter, S3AsyncClientOp}
 import org.apache.commons.codec.binary.Hex
@@ -32,6 +33,8 @@ trait S3StorageClient {
     (Stream.eval(parseBucket(bucket)), Stream.eval(parseFileKey(fileKey))).flatMapN(readFile)
 
   def readFile(bucket: BucketName, fileKey: FileKey): Stream[IO, Byte]
+
+  def readFileMultipart(bucket: BucketName, fileKey: FileKey): Stream[IO, Byte]
 
   def headObject(bucket: String, key: String): IO[HeadObject]
 
@@ -93,7 +96,8 @@ object S3StorageClient {
       .map(new S3StorageClientImpl(_, endpoint.toString))
 
   final class S3StorageClientImpl(client: S3AsyncClientOp[IO], val baseEndpoint: Uri) extends S3StorageClient {
-    private val s3: S3[IO] = S3.create(client)
+    private val s3: S3[IO]           = S3.create(client)
+    private val PartSize: PartSizeMB = refineMV(5)
 
     override def listObjectsV2(bucket: String): IO[ListObjectsV2Response] =
       client.listObjectsV2(ListObjectsV2Request.builder().bucket(bucket).build())
@@ -103,6 +107,9 @@ object S3StorageClient {
 
     override def readFile(bucket: BucketName, fileKey: FileKey): Stream[IO, Byte] =
       s3.readFile(bucket, fileKey)
+
+    override def readFileMultipart(bucket: BucketName, fileKey: FileKey): Stream[IO, Byte] =
+      s3.readFileMultipart(bucket, fileKey, PartSize)
 
     override def headObject(bucket: String, key: String): IO[HeadObject] =
       client
@@ -224,6 +231,9 @@ object S3StorageClient {
     override def listObjectsV2(bucket: BucketName, prefix: String): IO[ListObjectsV2Response] = raiseDisabledErr
 
     override def readFile(bucket: BucketName, fileKey: FileKey): Stream[IO, Byte] = Stream.raiseError[IO](disabledErr)
+
+    override def readFileMultipart(bucket: BucketName, fileKey: FileKey): Stream[IO, Byte] =
+      Stream.raiseError[IO](disabledErr)
 
     override def headObject(bucket: String, key: String): IO[HeadObject] = raiseDisabledErr
 
