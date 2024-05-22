c449eac7ad0820010ccdfd32f49671c8a65dca27
Simon Dumas <simon.dumas@epfl.ch>
2024-05-22 11:54:06 +0200

    Add slow query logger based on Doobie

---
+++ delta/sourcing-psql/src/main/scala/ch/epfl/bluebrain/nexus/delta/sourcing/QueryLogHandler.scala
@@ -0,0 +1,69 @@
+package ch.epfl.bluebrain.nexus.delta.sourcing
+
+import cats.effect.IO
+import ch.epfl.bluebrain.nexus.delta.kernel.Logger
+import ch.epfl.bluebrain.nexus.delta.sourcing.event.Event
+import ch.epfl.bluebrain.nexus.delta.sourcing.state.State
+import doobie.util.log
+import doobie.util.log.{LogEvent, LogHandler}
+import io.circe.Json
+
+import scala.concurrent.duration.FiniteDuration
+
+object QueryLogHandler {
+
+  private val logger = Logger[QueryLogHandler.type]
+
+  def apply(poolName: String, slowQueryThreshold: FiniteDuration): LogHandler[IO] = new LogHandler[IO] {
+    override def run(logEvent: LogEvent): IO[Unit] = logEvent match {
+      case log.Success(sql, args, label, exec, processing) if exec > slowQueryThreshold =>
+        logger.warn(s"""[$poolName] Slow Statement Execution:
+             |
+             | ${formatQuery(sql)}
+             |
+             | arguments = ${formatArguments(args)}
+             | label     = $label
+             | elapsed = ${exec.toMillis} ms exec + ${processing.toMillis} ms processing (${(exec + processing).toMillis} ms total)
+          """.stripMargin)
+      case log.Success(sql, args, label, exec, processing)                              =>
+        logger.debug(s"""[$poolName] Successful Statement Execution:
+             |
+             | ${formatQuery(sql)}
+             |
+             | arguments = ${formatArguments(args)}
+             | label     = $label
+             | elapsed = ${exec.toMillis} ms exec + ${processing.toMillis} ms processing (${(exec + processing).toMillis} ms total)
+          """.stripMargin)
+      case log.ProcessingFailure(sql, args, label, exec, processing, failure)           =>
+        logger.error(failure)(s"""[$poolName] Failed Resultset Processing:
+             |
+             | ${formatQuery(sql)}
+             |
+             | arguments = ${formatArguments(args)}
+             | label     = $label
+             | elapsed = ${exec.toMillis} ms exec + ${processing.toMillis} ms processing (failed) (${(exec + processing).toMillis.toString} ms total)
+          """.stripMargin)
+      case log.ExecFailure(sql, args, label, exec, failure)                             =>
+        logger.error(failure)(s"""[$poolName] Failed Statement Execution:
+             |
+             | ${formatQuery(sql)}
+             |
+             | arguments = ${formatArguments(args)}
+             | label     = $label
+             | elapsed = ${exec.toMillis} ms exec (failed)
+          """.stripMargin)
+    }
+
+    private def formatQuery(sql: String) = sql.linesIterator.dropWhile(_.trim.isEmpty).mkString("\n  ")
+
+    private def formatArguments(args: List[Any]) = args
+      .map {
+        case _: Json  => "{json blob}"
+        case e: Event => s"{event ${e.getClass.getSimpleName}}"
+        case s: State => s"{state ${s.getClass.getSimpleName}}"
+        case other    => other.toString
+      }
+      .mkString("[", ", ", "]")
+  }
+
+}
