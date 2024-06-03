0a2fb49df497608d3065112337f3069761ad9b98
Simon Dumas <simon.dumas@epfl.ch>
2024-06-03 16:56:43 +0200

    Patch project configuration and selfs in distribution

--- ship/src/main/scala/ch/epfl/bluebrain/nexus/ship/RunShip.scala
+++ ship/src/main/scala/ch/epfl/bluebrain/nexus/ship/RunShip.scala
@@ -62,7 +62,7 @@ object RunShip {
                     // Processors
           fileSelf                     = FileSelf(originalProjectContext)(originalBaseUri)
           sourcePatcher                = SourcePatcher(fileSelf, projectMapper, iriPatcher, targetBaseUri, eventClock, xas, config)
-          projectProcessor            <- ProjectProcessor(fetchActiveOrg, fetchContext, rcr, originalProjectContext, projectMapper, config, eventClock, xas)(targetBaseUri, jsonLdApi)
+          projectProcessor            <- ProjectProcessor(fetchActiveOrg, fetchContext, rcr, originalProjectContext, projectMapper, iriPatcher, config, eventClock, xas)(targetBaseUri, jsonLdApi)
           resolverProcessor            = ResolverProcessor(fetchContext, projectMapper, eventLogConfig, eventClock, xas)
           schemaProcessor              = SchemaProcessor(schemaLog, fetchContext, schemaImports, rcr, projectMapper, sourcePatcher, eventClock)
                     resourceProcessor            = ResourceProcessor(resourceLog, rcr, projectMapper, fetchContext, sourcePatcher, iriPatcher, config.resourceTypesToIgnore, eventClock)
