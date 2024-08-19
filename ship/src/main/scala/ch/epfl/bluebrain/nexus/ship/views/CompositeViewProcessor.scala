bcdbe1e7d2f48731bf2f8e0fa3b2c045823add79
Simon <simon.dumas@protonmail.com>
2024-08-19 15:07:37 +0200

    Skip default composite views during import (#5109)
    * Skip default composite views during import
    
    ---------
    
    Co-authored-by: Simon Dumas <simon.dumas@epfl.ch>

--- ship/src/main/scala/ch/epfl/bluebrain/nexus/ship/views/CompositeViewProcessor.scala
+++ ship/src/main/scala/ch/epfl/bluebrain/nexus/ship/views/CompositeViewProcessor.scala
@@ -6,6 +6,8 @@ import ch.epfl.bluebrain.nexus.delta.plugins.compositeviews.CompositeViews
 import ch.epfl.bluebrain.nexus.delta.plugins.compositeviews.model.CompositeViewEvent
 import ch.epfl.bluebrain.nexus.delta.plugins.compositeviews.model.CompositeViewEvent._
 import ch.epfl.bluebrain.nexus.delta.plugins.compositeviews.model.CompositeViewRejection.{IncorrectRev, ResourceAlreadyExists}
+import ch.epfl.bluebrain.nexus.delta.rdf.IriOrBNode.Iri
+import ch.epfl.bluebrain.nexus.delta.rdf.Vocabulary.nxv
 import ch.epfl.bluebrain.nexus.delta.rdf.jsonld.api.JsonLdApi
 import ch.epfl.bluebrain.nexus.delta.sdk.identities.model.Caller
 import ch.epfl.bluebrain.nexus.delta.sdk.projects.FetchContext
@@ -14,7 +16,7 @@ import ch.epfl.bluebrain.nexus.delta.sourcing.Transactors
 import ch.epfl.bluebrain.nexus.delta.sourcing.config.EventLogConfig
 import ch.epfl.bluebrain.nexus.delta.sourcing.model.EntityType
 import ch.epfl.bluebrain.nexus.delta.sourcing.model.Identity.Subject
-import ch.epfl.bluebrain.nexus.ship.views.CompositeViewProcessor.logger
+import ch.epfl.bluebrain.nexus.ship.views.CompositeViewProcessor.{defaultViewId, logger}
 import ch.epfl.bluebrain.nexus.ship.{EventClock, EventProcessor, ImportStatus, ProjectMapper}
 import io.circe.Decoder
 
@@ -38,29 +40,40 @@ class CompositeViewProcessor(views: UUID => IO[CompositeViews], projectMapper: P
     val cRev                = event.rev - 1
     val project             = projectMapper.map(event.project)
 
-    event match {
-      case e: CompositeViewCreated      => views(event.uuid).flatMap(_.create(project, e.source))
-      case e: CompositeViewUpdated      => views(event.uuid).flatMap(_.update(e.id, project, cRev, e.source))
-      case e: CompositeViewDeprecated   => views(event.uuid).flatMap(_.deprecate(e.id, project, cRev))
-      case e: CompositeViewUndeprecated => views(event.uuid).flatMap(_.undeprecate(e.id, project, cRev))
-      case _: CompositeViewTagAdded     => IO.unit // TODO: Can/should we tag?
+    if (event.id == defaultViewId) {
+      logger
+        .info(
+          s"Skipping default search views, they are updated independently to avoid putting pressure on the system at unexpected times"
+        )
+        .as(ImportStatus.Success)
+    } else {
+      val io = event match {
+        case e: CompositeViewCreated      => views(event.uuid).flatMap(_.create(project, e.source))
+        case e: CompositeViewUpdated      => views(event.uuid).flatMap(_.update(e.id, project, cRev, e.source))
+        case e: CompositeViewDeprecated   => views(event.uuid).flatMap(_.deprecate(e.id, project, cRev))
+        case e: CompositeViewUndeprecated => views(event.uuid).flatMap(_.undeprecate(e.id, project, cRev))
+        case _: CompositeViewTagAdded     => IO.unit // TODO: Can/should we tag?
+      }
+      io.redeemWith(
+        {
+          case a: ResourceAlreadyExists => logger.warn(a)("The resource already exists").as(ImportStatus.Dropped)
+          case i: IncorrectRev          =>
+            logger
+              .warn(i)(s"An incorrect revision has been provided for '${event.id}' in project '${event.project}'")
+              .as(ImportStatus.Dropped)
+          case other                    => IO.raiseError(other)
+        },
+        _ => IO.pure(ImportStatus.Success)
+      )
     }
-  }.redeemWith(
-    {
-      case a: ResourceAlreadyExists => logger.warn(a)("The resource already exists").as(ImportStatus.Dropped)
-      case i: IncorrectRev          =>
-        logger
-          .warn(i)(s"An incorrect revision has been provided for '${event.id}' in project '${event.project}'")
-          .as(ImportStatus.Dropped)
-      case other                    => IO.raiseError(other)
-    },
-    _ => IO.pure(ImportStatus.Success)
-  )
+  }
 }
 
 object CompositeViewProcessor {
 
   private val logger = Logger[CompositeViewProcessor]
+
+  private val defaultViewId: Iri = nxv + "searchView"
 
   def apply(
       fetchContext: FetchContext,
