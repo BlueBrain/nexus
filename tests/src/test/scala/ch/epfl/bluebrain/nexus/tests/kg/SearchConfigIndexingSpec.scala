b4f695dfd5c509b01d5de0bc1f12f0220f8b5fde
Cristina E. González-Espinoza <crisbeth46@gmail.com>
2024-08-23 12:16:22 +0200

    remove undeleted items from copy-pasting in test

--- tests/src/test/scala/ch/epfl/bluebrain/nexus/tests/kg/SearchConfigIndexingSpec.scala
+++ tests/src/test/scala/ch/epfl/bluebrain/nexus/tests/kg/SearchConfigIndexingSpec.scala
@@ -1144,7 +1144,7 @@ class SearchConfigIndexingSpec extends BaseIntegrationSpec {
       json"""{
                  "synaptomeSimulation": {
                     "injectionLocation": "soma[0]",
-                    "recordingLocation": ["soma[0]_0.5", "dend[38]_0.5", "dend[1]_0.4", "apic[53]_0.3", "dendrite.01A", "dendrite.01B"],
+                    "recordingLocation": ["soma[0]_0.5", "dend[38]_0.5", "dend[1]_0.4", "apic[53]_0.3"],
                     "synaptome": { "@id" : "https://bbp.epfl.ch/data/synapse/single-neuron-synaptome", "name" : "synaptome-model-04"}
                   }
                 }"""
