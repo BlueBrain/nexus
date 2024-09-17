ac5e2a7c9b6aed2b7bb62c6d203ff50ba5d2cabf
Simon <simon.dumas@protonmail.com>
2024-09-17 14:04:24 +0200

    Remove storage server implementation (#5144)
    Co-authored-by: Simon Dumas <simon.dumas@epfl.ch>

--- storage/src/main/scala/ch/epfl/bluebrain/nexus/storage/MediaTypes.scala
+++
@@ -1,9 +0,0 @@
-package ch.epfl.bluebrain.nexus.storage
-
-import akka.http.scaladsl.model.MediaType
-import akka.http.scaladsl.model.HttpCharsets.`UTF-8`
-
-object MediaTypes {
-  final val `application/ld+json`: MediaType.WithFixedCharset =
-    MediaType.applicationWithFixedCharset("ld+json", `UTF-8`, "jsonld")
-}
