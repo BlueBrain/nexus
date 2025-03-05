f255f8ba8ebac65c7635487d4a292536b7b5deb7
Simon <simon.dumas@protonmail.com>
2025-03-05 17:23:02 +0100

    Update to Localstack 4.2 (#5303)
    Co-authored-by: Simon Dumas <simon.dumas@senscience.ai>

--- delta/testkit/src/main/scala/ch/epfl/bluebrain/nexus/testkit/localstack/LocalStackS3.scala
+++ delta/testkit/src/main/scala/ch/epfl/bluebrain/nexus/testkit/localstack/LocalStackS3.scala
@@ -10,7 +10,7 @@ import software.amazon.awssdk.services.s3.S3AsyncClient
 
 object LocalStackS3 {
   val ServiceType = Service.S3
-  val Version     = "4.1"
+  val Version     = "4.2"
 
   def localstackS3(): Resource[IO, LocalStackV2Container] = {
 
