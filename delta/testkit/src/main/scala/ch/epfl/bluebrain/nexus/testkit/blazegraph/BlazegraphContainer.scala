876c90b3a2fb831e7bc799112f517da22ae3531e
Simon Dumas <simon.dumas@senscience.ai>
2025-04-04 09:44:03 +0200

    Allow more memory for Blazegraph in tests

--- delta/testkit/src/main/scala/ch/epfl/bluebrain/nexus/testkit/blazegraph/BlazegraphContainer.scala
+++ delta/testkit/src/main/scala/ch/epfl/bluebrain/nexus/testkit/blazegraph/BlazegraphContainer.scala
@@ -10,7 +10,7 @@ import scala.jdk.DurationConverters.ScalaDurationOps
 
 class BlazegraphContainer
     extends GenericContainer[BlazegraphContainer](DockerImageName.parse("bluebrain/blazegraph-nexus:2.1.6-RC-21-jre")) {
-  addEnv("JAVA_OPTS", "-Djava.awt.headless=true -XX:MaxDirectMemorySize=64m -Xmx256m -XX:+UseG1GC")
+  addEnv("JAVA_OPTS", "-Djava.awt.headless=true -XX:MaxDirectMemorySize=64m -Xmx512m -XX:+UseG1GC")
   addExposedPort(9999)
   setWaitStrategy(Wait.forHttp("/blazegraph").forStatusCode(200))
 }
