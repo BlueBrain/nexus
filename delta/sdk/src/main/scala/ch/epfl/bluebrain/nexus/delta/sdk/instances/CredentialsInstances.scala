7871918a8d33e40840934c203fb40fa8e81ef7af
Simon <simon.dumas@protonmail.com>
2025-05-05 16:41:25 +0200

    Update tests to use Elasticsearch 9 (#5341)
    Co-authored-by: Simon Dumas <simon.dumas@senscience.ai>

--- delta/sdk/src/main/scala/ch/epfl/bluebrain/nexus/delta/sdk/instances/CredentialsInstances.scala
+++ delta/sdk/src/main/scala/ch/epfl/bluebrain/nexus/delta/sdk/instances/CredentialsInstances.scala
@@ -1,14 +1,9 @@
 package ch.epfl.bluebrain.nexus.delta.sdk.instances
 
-import akka.http.scaladsl.model.headers.BasicHttpCredentials
 import org.http4s.BasicCredentials
 import pureconfig.*
-import pureconfig.generic.semiauto.deriveReader
 
 trait CredentialsInstances {
-
-  implicit final val basicHttpCredentialsConfigReader: ConfigReader[BasicHttpCredentials] =
-    deriveReader[BasicHttpCredentials]
 
   implicit final val basicCredentialsConfigReader: ConfigReader[BasicCredentials] =
     ConfigReader.forProduct2[BasicCredentials, String, String]("username", "password")(BasicCredentials(_, _))
