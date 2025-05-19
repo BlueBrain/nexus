17d0055f3bf5a1bfb7ca0214cada05a9bda3e6c0
Simon Dumas <simon.dumas@senscience.ai>
2025-05-19 16:27:29 +0200

    Scalafmt

--- delta/kernel/src/main/scala/ch/epfl/bluebrain/nexus/delta/kernel/http/circe/CirceEntityDecoder.scala
+++ delta/kernel/src/main/scala/ch/epfl/bluebrain/nexus/delta/kernel/http/circe/CirceEntityDecoder.scala
@@ -5,10 +5,10 @@ import io.circe.Decoder
 import org.http4s.EntityDecoder
 
 /**
- * Decoder which allows http4s to convert responses using jsoniter and circe
- * ported from the circe module of http4s
- * @see https://github.com/http4s/http4s
- */
+  * Decoder which allows http4s to convert responses using jsoniter and circe ported from the circe module of http4s
+  * @see
+  *   https://github.com/http4s/http4s
+  */
 trait CirceEntityDecoder {
   implicit def circeEntityDecoder[A: Decoder]: EntityDecoder[IO, A] = jsonOf[A]
 }
--- delta/kernel/src/main/scala/ch/epfl/bluebrain/nexus/delta/kernel/http/circe/CirceEntityEncoder.scala
+++ delta/kernel/src/main/scala/ch/epfl/bluebrain/nexus/delta/kernel/http/circe/CirceEntityEncoder.scala
@@ -5,10 +5,10 @@ import io.circe.Encoder
 import org.http4s.EntityEncoder
 
 /**
- * Encoder which allows http4s to convert to json using jsoniter and circe
- * ported from the circe module of http4s
- * @see https://github.com/http4s/http4s
- */
+  * Encoder which allows http4s to convert to json using jsoniter and circe ported from the circe module of http4s
+  * @see
+  *   https://github.com/http4s/http4s
+  */
 trait CirceEntityEncoder {
   implicit def circeEntityEncoder[A: Encoder]: EntityEncoder[IO, A] =
     jsonEncoderOf[A]
